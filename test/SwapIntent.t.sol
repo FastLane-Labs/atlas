18440ca8c2040c4ea96bbb1af17437cfae534dd0
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-08-11 20:26:04 +0200

    Fleshed out more data and transaction building in SwapIntent test

--- test/SwapIntent.t.sol
+++ test/SwapIntent.t.sol
@@ -4,43 +4,99 @@ pragma solidity ^0.8.18;
 import "forge-std/Test.sol";
 
 import {BaseTest} from "./base/BaseTest.t.sol";
+import {TxBuilder} from "../src/contracts/helpers/TxBuilder.sol";
 
-import {ProtocolCall} from "../src/contracts/types/CallTypes.sol";
+import {ProtocolCall, UserCall, SearcherCall} from "../src/contracts/types/CallTypes.sol";
+import {Verification} from "../src/contracts/types/VerificationTypes.sol";
 
 // QUESTIONS:
 // 1. What is escrowDuration (constructor arg in Atlas, Escrow.sol)? Where is it used?
+// 2. Is the Atlas contract always also the Escrow contract? Would Escrow ever be separate?
 
 // Refactor Ideas:
 // 1. Lots of bitwise operations explicitly coded in contracts - could be a helper lib thats more readable
+// 2. helper is currently a V2Helper and shared from BaseTest. Should only be in Uni V2 related tests
+// 3. Need a more generic helper for BaseTest
 
 
 contract SwapIntentTest is BaseTest {
 
+    TxBuilder public txBuilder;
 
     function setUp() public virtual override {
         BaseTest.setUp();
+
+        txBuilder = new TxBuilder({
+            protocolControl: address(control),
+            escrowAddress: address(escrow),
+            atlasAddress: address(atlas)
+        });
     }
 
     function testAtlasSwapUsingIntent() public {
         // Swap 10 WETH for 20 FXS
 
+        // Input params for Atlas.metacall() - will be populated below
+        ProtocolCall memory protocolCall;
+        UserCall memory userCall;
+        SearcherCall[] memory searcherCalls = new SearcherCall[](1); 
+        SearcherCall memory searcherCall; // First and only searcher will succeed
+        Verification memory verification; 
 
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
 
-        ProtocolCall memory protocolCall = helper.getProtocolCall();
-        // UserCall memory userCall = helper.buildUserCall(POOL_ONE, userEOA, TOKEN_ONE);
+        // uint8 v;
+        // bytes32 r;
+        // bytes32 s;
+
+        protocolCall = txBuilder.getProtocolCall();
+
+        // Builds the metaTx and to parts of userCall, signature still to be set
+        userCall = txBuilder.buildUserCall({
+            from: userEOA, // NOTE: Would from ever not be user?
+            to: address(atlas),
+            maxFeePerGas: 0, // TODO update
+            value: 0,
+            data: "" // TODO update
+        });
+
+        // Builds the SearcherCall
+        searcherCall = txBuilder.buildSearcherCall({
+            userCall: userCall,
+            protocolCall: protocolCall,
+            searcherCallData: "", // TODO update
+            searcherEOA: searcherOneEOA,
+            searcherContract: address(searcherOne),
+            bidAmount: 1e18
+        });
+
+        searcherCalls[0] = searcherCall;
+
+
+
 
         address executionEnvironment = atlas.createExecutionEnvironment(protocolCall);
         vm.label(address(executionEnvironment), "EXECUTION ENV");
 
 
         console.log("userEOA", userEOA);
-        console.log("atlas", address(atlas));
-        console.log("escrow", address(escrow));
+        console.log("atlas", address(atlas)); // ATLAS == ESCROW
+        console.log("escrow", address(escrow)); // ATLAS == ESCROW
         console.log("control", address(control));
         console.log("executionEnvironment", executionEnvironment);
+
+
+
+
+
+        vm.startPrank(userEOA);
+        // NOTE: Should metacall return something? Feels like a lot of data you might want to know about the tx
+        atlas.metacall({
+            protocolCall: protocolCall,
+            userCall: userCall,
+            searcherCalls: searcherCalls,
+            verification: verification
+        });
+        vm.stopPrank();
 
 
 
