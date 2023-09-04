5ee1d2ab615f22e8499f3101a05385da37948302
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-09-04 19:08:46 +0200

    Create test file for accounting concerns

---
+++ test/Accounting.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+import {BaseTest} from "./base/BaseTest.t.sol";
+
+
+contract Permit69Test is BaseTest {
+    function setUp() public virtual override {
+        BaseTest.setUp();
+    }
+    
+
+    function test1() public {
+
+    }
+
+}
