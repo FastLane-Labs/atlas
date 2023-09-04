ba973f78be3bcdb41bbd6e5ecefc4f7c1b6c3e3e
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-09-04 20:04:00 +0200

    Save progress on investigation

--- test/Accounting.t.sol
+++ test/Accounting.t.sol
@@ -12,9 +12,23 @@ contract Permit69Test is BaseTest {
     function setUp() public virtual override {
         BaseTest.setUp();
     }
-    
 
-    function test1() public {
+
+    function testSearcherEthValueIsNotDoubleCountedViaSurplusAccounting() public {
+
+        // TODO
+        // Define metacall where signer pays 2 ETH in msg.value
+        // Define searcher (attacker) who requires 1 ETH in their searcher value param
+        // Use SwapIntent as ProtocolControl because calls donateToBundler(searcherAddress)
+
+        // See donateToBundler() function in Escrow.sol
+        // which is called by Exec Env
+        // which delegatecalls donateToBundler in a ProtocolControl contract e.g. SwapIntent (_searcherPostCall)
+
+
+        // Criteria for this exploit to work:
+        // 1. Some ETH must be sent via msg.value in the metacall (???)
+        // 2. The ProtocolControl contract must call donateToBundler(searcherAddress)
 
     }
 
