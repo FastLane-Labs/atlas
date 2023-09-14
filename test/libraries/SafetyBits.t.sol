ac95efc245c2f6041163fd9a69a42065662afd48
jj1980a <j.feral@live.fr>
2023-09-14 09:50:19 +0800

    add constants test

--- test/libraries/SafetyBits.t.sol
+++ test/libraries/SafetyBits.t.sol
@@ -5,12 +5,104 @@ import "forge-std/Test.sol";
 
 import {SafetyBits} from "../../src/contracts/libraries/SafetyBits.sol";
 import "../../src/contracts/types/LockTypes.sol";
+import "../base/TestUtils.sol";
 
 contract SafetyBitsTest is Test {
     using SafetyBits for EscrowKey;
 
     function initializeEscrowLock() public pure returns (EscrowKey memory key) {
         key = key.initializeEscrowLock(true, 1, address(0));
+    }
+
+    function testConstants() public {
+        string memory expectedBitMapString = "0010000010001000";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._LOCKED_X_SEARCHERS_X_REQUESTED),
+            expectedBitMapString,
+            "_LOCKED_X_SEARCHERS_X_REQUESTED incorrect"
+        );
+
+        expectedBitMapString = "0100000010001000";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._LOCKED_X_SEARCHERS_X_VERIFIED),
+            expectedBitMapString,
+            "_LOCKED_X_SEARCHERS_X_VERIFIED incorrect"
+        );
+
+        expectedBitMapString = "0001000000100100";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._ACTIVE_X_STAGING_X_UNSET),
+            expectedBitMapString,
+            "_ACTIVE_X_STAGING_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001100000000010";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._PENDING_X_RELEASING_X_UNSET),
+            expectedBitMapString,
+            "_PENDING_X_RELEASING_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001000000101000";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._LOCKED_X_STAGING_X_UNSET),
+            expectedBitMapString,
+            "_LOCKED_X_STAGING_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001000001000100";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._ACTIVE_X_USER_X_UNSET),
+            expectedBitMapString,
+            "_ACTIVE_X_USER_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001000001001000";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._LOCKED_X_USER_X_UNSET),
+            expectedBitMapString,
+            "_LOCKED_X_USER_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001000010000010";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._PENDING_X_SEARCHER_X_UNSET),
+            expectedBitMapString,
+            "_PENDING_X_SEARCHER_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001000010000010";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._ACTIVE_X_SEARCHER_X_UNSET),
+            expectedBitMapString,
+            "_ACTIVE_X_SEARCHER_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001000100001000";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._LOCK_PAYMENTS), expectedBitMapString, "_LOCK_PAYMENTS incorrect"
+        );
+
+        expectedBitMapString = "0001010000000100";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._NO_SEARCHER_SUCCESS),
+            expectedBitMapString,
+            "_NO_SEARCHER_SUCCESS incorrect"
+        );
+
+        expectedBitMapString = "0001001000000010";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._ACTIVE_X_REFUND_X_UNSET),
+            expectedBitMapString,
+            "_ACTIVE_X_REFUND_X_UNSET incorrect"
+        );
+
+        expectedBitMapString = "0001010000001000";
+        assertEq(
+            TestUtils.uint16ToBinaryString(SafetyBits._LOCKED_X_VERIFICATION_X_UNSET),
+            expectedBitMapString,
+            "_LOCKED_X_VERIFICATION_X_UNSET incorrect"
+        );
     }
 
     function testInitializeEscrowLock() public {
@@ -87,7 +179,7 @@ contract SafetyBitsTest is Test {
     function testTurnSearcherLock() public {
         EscrowKey memory key = initializeEscrowLock();
         key = key.turnSearcherLock(address(1));
-        assertTrue(key.lockState == SafetyBits._LOCKED_X_SEARCHERS_X_REQUESTED);
+        assertTrue(key.lockState == SafetyBits._LOCKED_X_SEARCHERS_X_VERIFIED);
         assertTrue(key.approvedCaller == address(1));
     }
 }
