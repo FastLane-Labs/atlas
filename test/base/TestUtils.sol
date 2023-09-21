cf5e5c6ae7a6b98491f104fcad41287b188e4fea
jj1980a <j.feral@live.fr>
2023-09-21 09:05:18 +0800

    initial commit

--- test/base/TestUtils.sol
+++ test/base/TestUtils.sol
@@ -25,6 +25,29 @@ library TestUtils {
         return string(output);
     }
 
+    // String <> uint32 binary Converter Utility
+    function uint32ToBinaryString(uint32 n) public pure returns (string memory) {
+        uint256 newN = uint256(n);
+        // revert on out of range input
+        require(newN < 4294967296, "n too large");
+
+        bytes memory output = new bytes(32);
+
+        uint256 i = 0;
+        for (; i < 32; i++) {
+            if (newN == 0) {
+                // Now that we've filled in the last 1, fill rest of 0s in
+                for (; i < 32; i++) {
+                    output[31 - i] = bytes1("0");
+                }
+                break;
+            }
+            output[31 - i] = (newN % 2 == 1) ? bytes1("1") : bytes1("0");
+            newN /= 2;
+        }
+        return string(output);
+    }
+
     // String <> uint256 binary Converter Utility
     function uint256ToBinaryString(uint256 n) public pure returns (string memory) {
         bytes memory output = new bytes(256);
