f638abfb266432111d31499ce514710fbb311e0c
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-09-27 15:47:35 +0200

    Added deploy script for Atlas + SwapIntent

--- script/deploy-atlas.s.sol
+++ script/deploy-atlas.s.sol
@@ -5,17 +5,17 @@ import "forge-std/Script.sol";
 import "forge-std/Test.sol";
 
 import {Atlas} from "src/contracts/atlas/Atlas.sol";
-
+import {SwapIntentController} from "src/contracts/examples/intents-example/SwapIntent.sol";
+import {SimpleRFQSolver} from "test/SwapIntent.t.sol"; // TODO move to searcher script
 
 contract DeployAtlasScript is Script {
-
     Atlas public atlas;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("GOV_PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
 
-        console.log("Deployer address: ", deployer);
+        console.log("Deployer address: \t\t\t\t", deployer);
 
         vm.startBroadcast(deployerPrivateKey);
 
@@ -23,6 +23,35 @@ contract DeployAtlasScript is Script {
 
         vm.stopBroadcast();
 
-        console.log("Atlas deployed at: ", address(atlas));
+        console.log("Atlas deployed at: \t\t\t\t", address(atlas));
+    }
+}
+
+contract DeployAtlasAndSwapIntentDAppControlScript is Script {
+    Atlas public atlas;
+    SwapIntentController public swapIntentControl;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("GOV_PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+
+        console.log("Deployer address: \t\t\t\t", deployer);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the Atlas contract
+        atlas = new Atlas(64);
+
+        // Deploy the SwapIntent DAppControl contract
+        swapIntentControl = new SwapIntentController(address(atlas));
+
+        // Integrate SwapIntent with Atlas
+        atlas.initializeGovernance(address(swapIntentControl));
+        atlas.integrateDApp(address(swapIntentControl), address(swapIntentControl));
+
+        vm.stopBroadcast();
+
+        console.log("Atlas deployed at: \t\t\t\t", address(atlas));
+        console.log("SwapIntent DAppControl deployed at: \t\t", address(swapIntentControl));
     }
 }
