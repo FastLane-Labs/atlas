2d56559648f38c6f079a4e368399848cbf1a21e6
Alex <dalexwatts@gmail.com>
2023-10-21 20:21:53 -0700

    updated the mimic / factory

--- src/contracts/atlas/AtlETH.sol
+++ src/contracts/atlas/AtlETH.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.19;
+pragma solidity 0.8.18;
 
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
--- test/MainTest.t.sol
+++ test/MainTest.t.sol
@@ -6,6 +6,7 @@ import {SafeTransferLib, ERC20} from "solmate/utils/SafeTransferLib.sol";
 import {IEscrow} from "../src/contracts/interfaces/IEscrow.sol";
 import {IAtlas} from "../src/contracts/interfaces/IAtlas.sol";
 import {IDAppIntegration} from "../src/contracts/interfaces/IDAppIntegration.sol";
+import {IExecutionEnvironment} from "../src/contracts/interfaces/IExecutionEnvironment.sol";
 
 import {Atlas} from "../src/contracts/atlas/Atlas.sol";
 import {Mimic} from "../src/contracts/atlas/Mimic.sol";
@@ -162,17 +163,21 @@ contract MainTest is BaseTest {
         */
     }
 
-    /*
+    
     function testMimic() public {
-        address aaaaa = address(this);
+        
+        // uncomment to debug if this test is broken
+        /*
+        address aaaaa = atlas.executionTemplate();
         address bbbbb = msg.sender;
         address ccccc = address(this);
-        uint16 ddddd = uint16(0x1111);
+        uint32 ddddd = uint32(0x11111111);
         bytes32 eeeee = keccak256(abi.encodePacked(uint256(0x2222)));
         // Mimic mimic = new Mimic();
         //console.log("----");
         //console.log("runtime code:");
         //console.logBytes(address(mimic).code);
+        
         console.log("aaaaa", aaaaa);
         console.log("bbbbb", bbbbb);
         console.log("ccccc", ccccc);
@@ -184,33 +189,26 @@ contract MainTest is BaseTest {
 
         bytes memory creationCode = type(Mimic).creationCode;
         //bytes memory creationCode = new bytes(790);
-        assembly {
-            mstore(add(creationCode, 85), add(
-                shl(96, aaaaa), 
-                0x73ffffffffffffffffffffff
-            ))
-            mstore(add(creationCode, 131), add(
-                shl(96, bbbbb), 
-                0x73ffffffffffffffffffffff
-            ))
-            mstore(add(creationCode, 152), add(
-                shl(96, ccccc), 
-                add(
-                    add(
-                        shl(88, 0x61), 
-                        shl(72, ddddd)
-                    ),
-                    0x7f0000000000000000
-                )
-            ))
-            mstore(add(creationCode, 176), eeeee)
-        }
+        creationCode = atlas.getMimicCreationCode(aaaaa, ddddd, bbbbb, eeeee);
+        
         
         console.log("assembly modified code:");
         console.logBytes(creationCode);
         console.log("----");
+        */
+        
+
+        vm.startPrank(userEOA);
+        IAtlas(address(atlas)).createExecutionEnvironment(address(control));
+        address newEnvironment = IAtlas(address(atlas)).createExecutionEnvironment(address(control));
+        vm.stopPrank();
+
+        assertTrue(IExecutionEnvironment(newEnvironment).getUser() == userEOA, "Mimic Error - User Mismatch");
+        assertTrue(IExecutionEnvironment(newEnvironment).getControl() == address(control), "Mimic Error - Control Mismatch");
+        assertTrue(IExecutionEnvironment(newEnvironment).getConfig() == control.callConfig(), "Mimic Error - CallConfig Mismatch");
+        assertTrue(IExecutionEnvironment(newEnvironment).getEscrow() == address(atlas), "Mimic Error - Escrow/Atlas Address Mismatch");
     }
-    */
+    
 
     function testTestUserOperation() public {
         uint8 v;
