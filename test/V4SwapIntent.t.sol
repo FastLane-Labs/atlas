10c95774759f3f7e698af4701a45d2e133636909
0x2 <75550506+Code0x2@users.noreply.github.com>
2023-11-07 20:47:18 -0800

    version bunp

--- test/V4SwapIntent.t.sol
+++ test/V4SwapIntent.t.sol
@@ -18,7 +18,8 @@ import {SolverBase} from "../src/contracts/solver/SolverBase.sol";
 import {PoolManager} from "v4-core/PoolManager.sol";
 
 contract V4SwapIntentTest is BaseTest {
-    SwapIntentController public swapIntentController;
+    V4SwapIntentController public swapIntentController;
+    PoolManager public poolManager;
     TxBuilder public txBuilder;
     Sig public sig;
 
@@ -50,126 +51,129 @@ contract V4SwapIntentTest is BaseTest {
             atlasAddress: address(atlas)
         });
 
+        // Deploy new poolamanger and create a DAI/WETH pool with no hooks
+        poolManager = new PoolManager(30000000);
+
         // Deposit ETH from Searcher signer to pay for searcher's gas 
         // vm.prank(solverOneEOA); 
         // atlas.deposit{value: 1e18}();
     }
 
-    function testAtlasV4SwapIntentWithUniswapSolver() public {
-        // Swap 10 WETH to 100 DAI
+    // function testAtlasV4SwapIntentWithUniswapSolver() public {
+    //     // Swap 10 WETH to 100 DAI
 
-        // Solver deploys the RFQ solver contract (defined at bottom of this file)
-        vm.startPrank(solverOneEOA);
-        UniswapIntentSolver uniswapSolver = new UniswapIntentSolver(address(atlas));
-        deal(WETH_ADDRESS, address(uniswapSolver), 1e18); // 1 WETH to solver to pay bid
-        atlas.deposit{value: 1e18}();
-        vm.stopPrank();
+    //     // Solver deploys the RFQ solver contract (defined at bottom of this file)
+    //     vm.startPrank(solverOneEOA);
+    //     UniswapIntentSolver uniswapSolver = new UniswapIntentSolver(address(atlas));
+    //     deal(WETH_ADDRESS, address(uniswapSolver), 1e18); // 1 WETH to solver to pay bid
+    //     atlas.deposit{value: 1e18}();
+    //     vm.stopPrank();
 
-        // Input params for Atlas.metacall() - will be populated below
-        UserOperation memory userOp;
-        SolverOperation[] memory solverOps = new SolverOperation[](1);
-        DAppOperation memory dAppOp;
+    //     // Input params for Atlas.metacall() - will be populated below
+    //     UserOperation memory userOp;
+    //     SolverOperation[] memory solverOps = new SolverOperation[](1);
+    //     DAppOperation memory dAppOp;
 
-        vm.startPrank(userEOA);
-        address executionEnvironment = atlas.createExecutionEnvironment(txBuilder.control());
-        console.log("executionEnvironment a",executionEnvironment);
-        vm.stopPrank();
-        vm.label(address(executionEnvironment), "EXECUTION ENV");
+    //     vm.startPrank(userEOA);
+    //     address executionEnvironment = atlas.createExecutionEnvironment(txBuilder.control());
+    //     console.log("executionEnvironment a",executionEnvironment);
+    //     vm.stopPrank();
+    //     vm.label(address(executionEnvironment), "EXECUTION ENV");
 
-        // userOpData is used in delegatecall from exec env to control, calling preOpsCall
-        // first 4 bytes are "userSelector" param in preOpsCall in DAppControl - swap() selector
-        // rest of data is "userData" param
+    //     // userOpData is used in delegatecall from exec env to control, calling preOpsCall
+    //     // first 4 bytes are "userSelector" param in preOpsCall in DAppControl - swap() selector
+    //     // rest of data is "userData" param
         
-        // swap(SwapIntent calldata) selector = 0x98434997
-        bytes memory userOpData = abi.encodeWithSelector(SwapIntentController.swap.selector, swapIntent);
+    //     // swap(SwapIntent calldata) selector = 0x98434997
+    //     bytes memory userOpData = abi.encodeWithSelector(SwapIntentController.swap.selector, swapIntent);
 
-        // Builds the metaTx and to parts of userOp, signature still to be set
-        userOp = txBuilder.buildUserOperation({
-            from: userEOA, // NOTE: Would from ever not be user?
-            to: address(swapIntentController),
-            maxFeePerGas: tx.gasprice + 1, // TODO update
-            value: 0,
-            deadline: block.number + 2,
-            data: userOpData
-        });
+    //     // Builds the metaTx and to parts of userOp, signature still to be set
+    //     userOp = txBuilder.buildUserOperation({
+    //         from: userEOA, // NOTE: Would from ever not be user?
+    //         to: address(swapIntentController),
+    //         maxFeePerGas: tx.gasprice + 1, // TODO update
+    //         value: 0,
+    //         deadline: block.number + 2,
+    //         data: userOpData
+    //     });
 
-        // User signs the userOp
-        // user doees NOT sign the userOp when they are bundling
-        // (sig.v, sig.r, sig.s) = vm.sign(userPK, atlas.getUserOperationPayload(userOp));
-        // userOp.signature = abi.encodePacked(sig.r, sig.s, sig.v);
+    //     // User signs the userOp
+    //     // user doees NOT sign the userOp when they are bundling
+    //     // (sig.v, sig.r, sig.s) = vm.sign(userPK, atlas.getUserOperationPayload(userOp));
+    //     // userOp.signature = abi.encodePacked(sig.r, sig.s, sig.v);
 
-        // Build solver calldata (function selector on solver contract and its params)
-        bytes memory solverOpData = abi.encodeWithSelector(
-            UniswapIntentSolver.fulfillWithSwap.selector, 
-            swapIntent,
-            executionEnvironment
-        );
+    //     // Build solver calldata (function selector on solver contract and its params)
+    //     bytes memory solverOpData = abi.encodeWithSelector(
+    //         UniswapIntentSolver.fulfillWithSwap.selector, 
+    //         swapIntent,
+    //         executionEnvironment
+    //     );
 
-        // Builds the SolverOperation
-        solverOps[0] = txBuilder.buildSolverOperation({
-            userOp: userOp,
-            solverOpData: solverOpData,
-            solverEOA: solverOneEOA,
-            solverContract: address(uniswapSolver),
-            bidAmount: 1e18
-        });
+    //     // Builds the SolverOperation
+    //     solverOps[0] = txBuilder.buildSolverOperation({
+    //         userOp: userOp,
+    //         solverOpData: solverOpData,
+    //         solverEOA: solverOneEOA,
+    //         solverContract: address(uniswapSolver),
+    //         bidAmount: 1e18
+    //     });
 
-        // Solver signs the solverOp
-        (sig.v, sig.r, sig.s) = vm.sign(solverOnePK, atlas.getSolverPayload(solverOps[0]));
-        solverOps[0].signature = abi.encodePacked(sig.r, sig.s, sig.v);
+    //     // Solver signs the solverOp
+    //     (sig.v, sig.r, sig.s) = vm.sign(solverOnePK, atlas.getSolverPayload(solverOps[0]));
+    //     solverOps[0].signature = abi.encodePacked(sig.r, sig.s, sig.v);
 
-        // Frontend creates dAppOp calldata after seeing rest of data
-        dAppOp = txBuilder.buildDAppOperation(governanceEOA, userOp, solverOps);
+    //     // Frontend creates dAppOp calldata after seeing rest of data
+    //     dAppOp = txBuilder.buildDAppOperation(governanceEOA, userOp, solverOps);
 
-        // Frontend signs the dAppOp payload
-        (sig.v, sig.r, sig.s) = vm.sign(governancePK, atlas.getDAppOperationPayload(dAppOp));
-        dAppOp.signature = abi.encodePacked(sig.r, sig.s, sig.v);
+    //     // Frontend signs the dAppOp payload
+    //     (sig.v, sig.r, sig.s) = vm.sign(governancePK, atlas.getDAppOperationPayload(dAppOp));
+    //     dAppOp.signature = abi.encodePacked(sig.r, sig.s, sig.v);
 
-        // Check user token balances before
-        uint256 userWethBalanceBefore = WETH.balanceOf(userEOA);
-        uint256 userDaiBalanceBefore = DAI.balanceOf(userEOA);
+    //     // Check user token balances before
+    //     uint256 userWethBalanceBefore = WETH.balanceOf(userEOA);
+    //     uint256 userDaiBalanceBefore = DAI.balanceOf(userEOA);
 
-        vm.prank(userEOA); // Burn all users WETH except 10 so logs are more readable
-        WETH.transfer(address(1), userWethBalanceBefore - swapIntent.amountUserSells);
-        userWethBalanceBefore = WETH.balanceOf(userEOA);
+    //     vm.prank(userEOA); // Burn all users WETH except 10 so logs are more readable
+    //     WETH.transfer(address(1), userWethBalanceBefore - swapIntent.amountUserSells);
+    //     userWethBalanceBefore = WETH.balanceOf(userEOA);
 
-        assertTrue(userWethBalanceBefore >= swapIntent.amountUserSells, "Not enough starting WETH");
+    //     assertTrue(userWethBalanceBefore >= swapIntent.amountUserSells, "Not enough starting WETH");
 
-        console.log("\nBEFORE METACALL");
-        console.log("User WETH balance", WETH.balanceOf(userEOA));
-        console.log("User DAI balance", DAI.balanceOf(userEOA));
-        console.log("Solver WETH balance", WETH.balanceOf(address(uniswapSolver)));
-        console.log("Solver DAI balance", DAI.balanceOf(address(uniswapSolver)));
+    //     console.log("\nBEFORE METACALL");
+    //     console.log("User WETH balance", WETH.balanceOf(userEOA));
+    //     console.log("User DAI balance", DAI.balanceOf(userEOA));
+    //     console.log("Solver WETH balance", WETH.balanceOf(address(uniswapSolver)));
+    //     console.log("Solver DAI balance", DAI.balanceOf(address(uniswapSolver)));
 
-        vm.startPrank(userEOA);
+    //     vm.startPrank(userEOA);
 
-        assertFalse(simulator.simUserOperation(userOp), "metasimUserOperationcall tested true");
+    //     assertFalse(simulator.simUserOperation(userOp), "metasimUserOperationcall tested true");
         
-        WETH.approve(address(atlas), swapIntent.amountUserSells);
+    //     WETH.approve(address(atlas), swapIntent.amountUserSells);
 
-        assertTrue(simulator.simUserOperation(userOp), "metasimUserOperationcall tested false");
+    //     assertTrue(simulator.simUserOperation(userOp), "metasimUserOperationcall tested false");
 
-        // Check solver does NOT have DAI - it must use Uniswap to get it during metacall
-        assertEq(DAI.balanceOf(address(uniswapSolver)), 0, "Solver has DAI before metacall");
+    //     // Check solver does NOT have DAI - it must use Uniswap to get it during metacall
+    //     assertEq(DAI.balanceOf(address(uniswapSolver)), 0, "Solver has DAI before metacall");
 
-        // NOTE: Should metacall return something? Feels like a lot of data you might want to know about the tx
-        atlas.metacall({
-            userOp: userOp,
-            solverOps: solverOps,
-            dAppOp: dAppOp
-        });
-        vm.stopPrank();
+    //     // NOTE: Should metacall return something? Feels like a lot of data you might want to know about the tx
+    //     atlas.metacall({
+    //         userOp: userOp,
+    //         solverOps: solverOps,
+    //         dAppOp: dAppOp
+    //     });
+    //     vm.stopPrank();
 
-        console.log("\nAFTER METACALL");
-        console.log("User WETH balance", WETH.balanceOf(userEOA));
-        console.log("User DAI balance", DAI.balanceOf(userEOA));
-        console.log("Solver WETH balance", WETH.balanceOf(address(uniswapSolver)));
-        console.log("Solver DAI balance", DAI.balanceOf(address(uniswapSolver)));
+    //     console.log("\nAFTER METACALL");
+    //     console.log("User WETH balance", WETH.balanceOf(userEOA));
+    //     console.log("User DAI balance", DAI.balanceOf(userEOA));
+    //     console.log("Solver WETH balance", WETH.balanceOf(address(uniswapSolver)));
+    //     console.log("Solver DAI balance", DAI.balanceOf(address(uniswapSolver)));
 
-        // Check user token balances after
-        assertEq(WETH.balanceOf(userEOA), userWethBalanceBefore - swapIntent.amountUserSells, "Did not spend enough WETH");
-        assertEq(DAI.balanceOf(userEOA), userDaiBalanceBefore + swapIntent.amountUserBuys, "Did not receive enough DAI");
-    }
+    //     // Check user token balances after
+    //     assertEq(WETH.balanceOf(userEOA), userWethBalanceBefore - swapIntent.amountUserSells, "Did not spend enough WETH");
+    //     assertEq(DAI.balanceOf(userEOA), userDaiBalanceBefore + swapIntent.amountUserBuys, "Did not receive enough DAI");
+    // }
 }
 
 contract UniswapV4IntentSolver is SolverBase {
