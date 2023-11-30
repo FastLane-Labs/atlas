60a6f7e682a73a5bb2465e7a784ca57f5264cdac
Jordan Hagan <jordan@ephess.nz>
2023-12-01 10:49:44 +1100

    fix lint errors

--- test/Storage.t.sol
+++ test/Storage.t.sol
@@ -39,9 +39,9 @@ contract StorageTest is Test {
 
         // look up the storage slots so that we can make sure they don't change by accident
         
-        uint256 totalSupplySlot = stdstore.target(address(s)).sig('totalSupply()').find();
-        uint256 noncesSlot = stdstore.target(address(s)).sig('nonces(address)').with_key(address(this)).find();
-        uint256 lockSlot = stdstore.target(address(s)).sig('lock()').find();
+        uint256 totalSupplySlot = stdstore.target(address(s)).sig("totalSupply()").find();
+        uint256 noncesSlot = stdstore.target(address(s)).sig("nonces(address)").with_key(address(this)).find();
+        uint256 lockSlot = stdstore.target(address(s)).sig("lock()").find();
 
         // TODO: figure out how to check the allowance and ledger slots, haven't been able to make these work yet
 
@@ -49,7 +49,7 @@ contract StorageTest is Test {
         // and you either need to update the slot number or revert the change
 
         assertEq(totalSupplySlot, 0);
-        assertEq(noncesSlot, 49784443915320261189887103614045882155521089248264299114442679287293484801912);
+        assertEq(noncesSlot, 49_784_443_915_320_261_189_887_103_614_045_882_155_521_089_248_264_299_114_442_679_287_293_484_801_912);
         assertEq(lockSlot, 4);
     }
 }
