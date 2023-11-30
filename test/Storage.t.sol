ab05568a70506fbe8b5f231d5ab12ca191a9c2a0
Jordan Hagan <jordan@ephess.nz>
2023-12-01 10:53:07 +1100

    fix formatting again

--- test/Storage.t.sol
+++ test/Storage.t.sol
@@ -7,7 +7,6 @@ import { Storage } from "src/contracts/atlas/Storage.sol";
 
 using stdStorage for StdStorage;
 
-
 contract StorageTest is Test {
     function testNewStorage() public {
         Storage s = new Storage(
@@ -38,7 +37,7 @@ contract StorageTest is Test {
         );
 
         // look up the storage slots so that we can make sure they don't change by accident
-        
+
         uint256 totalSupplySlot = stdstore.target(address(s)).sig("totalSupply()").find();
         uint256 noncesSlot = stdstore.target(address(s)).sig("nonces(address)").with_key(address(this)).find();
         uint256 lockSlot = stdstore.target(address(s)).sig("lock()").find();
@@ -49,7 +48,10 @@ contract StorageTest is Test {
         // and you either need to update the slot number or revert the change
 
         assertEq(totalSupplySlot, 0);
-        assertEq(noncesSlot, 49_784_443_915_320_261_189_887_103_614_045_882_155_521_089_248_264_299_114_442_679_287_293_484_801_912);
+        assertEq(
+            noncesSlot,
+            49_784_443_915_320_261_189_887_103_614_045_882_155_521_089_248_264_299_114_442_679_287_293_484_801_912
+        );
         assertEq(lockSlot, 4);
     }
 }
