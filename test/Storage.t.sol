922963c98daee78ee133f79e86f0ccd505e714eb
Jordan Hagan <jordan@ephess.nz>
2023-12-08 11:53:34 +1100

    Tested internal constructor variables

--- test/Storage.t.sol
+++ test/Storage.t.sol
@@ -9,7 +9,7 @@ using stdStorage for StdStorage;
 
 contract StorageTest is Test {
     function testNewStorage() public {
-        Storage s = new Storage(
+        MockStorageTests s = new MockStorageTests(
             1, // _escrowDuration
             address(1), // _factory
             address(2), // _verification
@@ -24,6 +24,8 @@ contract StorageTest is Test {
         assertEq(s.GAS_ACC_LIB(), address(3));
         assertEq(s.SAFETY_LOCKS_LIB(), address(4));
         assertEq(s.SIMULATOR(), address(5));
+        assertEq(s.getInitialChainId(), block.chainid);
+        assertEq(s.getInitialDomainSeparator(), bytes32("SEPARATOR"));
     }
 
     function testStorageSlotsDontChange() public {
@@ -53,5 +55,30 @@ contract StorageTest is Test {
             49_784_443_915_320_261_189_887_103_614_045_882_155_521_089_248_264_299_114_442_679_287_293_484_801_912
         );
         assertEq(lockSlot, 4);
+    }
+}
+
+contract MockStorageTests is Storage {
+    constructor(
+        uint256 _escrowDuration,
+        address _factory,
+        address _verification,
+        address _gasAccLib,
+        address _safetyLocksLib,
+        address _simulator
+    )
+        Storage(_escrowDuration, _factory, _verification, _gasAccLib, _safetyLocksLib, _simulator)
+    { }
+
+    function getInitialChainId() public view returns (uint256) {
+        return INITIAL_CHAIN_ID;
+    }
+
+    function getInitialDomainSeparator() public view returns (bytes32) {
+        return INITIAL_DOMAIN_SEPARATOR;
+    }
+
+    function _computeDomainSeparator() override internal view virtual returns (bytes32) {
+        return bytes32("SEPARATOR");
     }
 }
