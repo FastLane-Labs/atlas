cb56fc723eb81ddf3affd6fb54237033ac5aa75c
jj1980a <j.feral@live.fr>
2023-12-14 20:58:48 +0800

    userWrapper tests, refactor MockDAppControl

--- test/ExecutionEnvironment.t.sol
+++ test/ExecutionEnvironment.t.sol
@@ -5,34 +5,16 @@ import "forge-std/Test.sol";
 import { BaseTest } from "./base/BaseTest.t.sol";
 
 import { ExecutionEnvironment } from "../src/contracts/atlas/ExecutionEnvironment.sol";
-import { DummyDAppControl } from "./base/DummyDAppControl.sol";
+import { DAppControl } from "../src/contracts/dapp/DAppControl.sol";
 
 import { IFactory } from "../src/contracts/interfaces/IFactory.sol";
 
 import { SafetyBits } from "../src/contracts/libraries/SafetyBits.sol";
 
+import "../src/contracts/types/DAppApprovalTypes.sol";
 import "../src/contracts/types/UserCallTypes.sol";
+import "../src/contracts/types/SolverCallTypes.sol";
 import "../src/contracts/types/LockTypes.sol";
-
-contract MockDAppControl is DummyDAppControl {
-    constructor(address escrow) DummyDAppControl(escrow) { }
-
-    function _preOpsCall(UserOperation calldata userOp) internal pure override returns (bytes memory) {
-        if (userOp.data.length > 0) {
-            (bytes4 selector, bytes memory data) = abi.decode(userOp.data, (bytes4, bytes));
-            if (selector == this.testPreOpsWrapper.selector) {
-                return testPreOpsWrapper(data);
-            }
-        }
-        return new bytes(0);
-    }
-
-    function testPreOpsWrapper(bytes memory data) public pure returns (bytes memory) {
-        (bool shouldRevert, uint256 returnValue) = abi.decode(data, (bool, uint256));
-        require(!shouldRevert);
-        return abi.encode(returnValue);
-    }
-}
 
 contract ExecutionEnvironmentTest is BaseTest {
     using SafetyBits for EscrowKey;
@@ -42,15 +24,14 @@ contract ExecutionEnvironmentTest is BaseTest {
 
     EscrowKey public escrowKey;
 
+    address public constant governance = address(888);
     address public constant user = address(999);
 
-    function setUp() public override {
-        super.setUp();
+    CallConfig private callConfig;
 
-        address governance = address(888);
-
-        vm.startPrank(address(governance));
-        dAppControl = new MockDAppControl(escrow);
+    function setupDAppControl(CallConfig memory customCallConfig) internal {
+        vm.startPrank(governance);
+        dAppControl = new MockDAppControl(escrow, governance, customCallConfig);
         atlasVerification.initializeGovernance(address(dAppControl));
         atlasVerification.integrateDApp(address(dAppControl));
         vm.stopPrank();
@@ -64,6 +45,8 @@ contract ExecutionEnvironmentTest is BaseTest {
         UserOperation memory userOp;
         bytes memory preOpsData;
         bool status;
+
+        setupDAppControl(callConfig);
 
         // Valid
         userOp.from = user;
@@ -102,6 +85,8 @@ contract ExecutionEnvironmentTest is BaseTest {
         UserOperation memory userOp;
         bytes memory preOpsData;
         bool status;
+
+        setupDAppControl(callConfig);
 
         userOp.from = user;
         userOp.to = address(atlas);
@@ -144,6 +129,8 @@ contract ExecutionEnvironmentTest is BaseTest {
         bytes memory userData;
         bool status;
 
+        setupDAppControl(callConfig);
+
         userOp.from = user;
         userOp.to = address(atlas);
 
@@ -169,12 +156,15 @@ contract ExecutionEnvironmentTest is BaseTest {
         bool status;
         bytes memory data;
 
+        setupDAppControl(callConfig);
+
         userOp.from = user;
         userOp.to = address(atlas);
+        userOp.dapp = address(dAppControl);
 
         // Valid
         uint256 expectedReturnValue = 123;
-        userOp.data = abi.encode(dAppControl.testPreOpsWrapper.selector, abi.encode(false, expectedReturnValue));
+        userOp.data = abi.encodeWithSelector(dAppControl.mockOperation.selector, false, expectedReturnValue);
         escrowKey = escrowKey.holdPreOpsLock(address(dAppControl));
         preOpsData = abi.encodeWithSelector(executionEnvironment.preOpsWrapper.selector, userOp);
         preOpsData = abi.encodePacked(preOpsData, escrowKey.pack());
@@ -184,13 +174,121 @@ contract ExecutionEnvironmentTest is BaseTest {
         assertEq(abi.decode(abi.decode(data, (bytes)), (uint256)), expectedReturnValue);
 
         // DelegateRevert
-        userOp.data = abi.encode(dAppControl.testPreOpsWrapper.selector, abi.encode(true, 0));
+        userOp.data = abi.encodeWithSelector(dAppControl.mockOperation.selector, true, uint256(0));
         escrowKey = escrowKey.holdPreOpsLock(address(dAppControl));
         preOpsData = abi.encodeWithSelector(executionEnvironment.preOpsWrapper.selector, userOp);
         preOpsData = abi.encodePacked(preOpsData, escrowKey.pack());
         vm.prank(address(atlas));
         vm.expectRevert(bytes("ERR-EC02 DelegateRevert"));
         (status,) = address(executionEnvironment).call(preOpsData);
-        assertTrue(status, "expectRevert ERR-EB01 InvalidSender: call did not revert");
+        assertTrue(status, "expectRevert ERR-EC02 DelegateRevert: call did not revert");
+    }
+
+    function test_userWrapper() public {
+        UserOperation memory userOp;
+        bytes memory userData;
+        bool status;
+        bytes memory data;
+        uint256 expectedReturnValue;
+
+        setupDAppControl(callConfig);
+
+        userOp.from = user;
+        userOp.to = address(atlas);
+        userOp.dapp = address(dAppControl);
+
+        // ValueExceedsBalance
+        userOp.value = 1; // Positive value but EE has no balance
+        escrowKey = escrowKey.holdUserLock(address(dAppControl));
+        userData = abi.encodeWithSelector(executionEnvironment.userWrapper.selector, userOp);
+        userData = abi.encodePacked(userData, escrowKey.pack());
+        vm.prank(address(atlas));
+        vm.expectRevert(bytes("ERR-CE01 ValueExceedsBalance"));
+        (status,) = address(executionEnvironment).call(userData);
+        assertTrue(status, "expectRevert ERR-CE01 ValueExceedsBalance: call did not revert");
+        userOp.value = 0;
+
+        // Valid (needsDelegateUser=false)
+        expectedReturnValue = 987;
+        userOp.data = abi.encodeWithSelector(dAppControl.mockOperation.selector, false, expectedReturnValue);
+        escrowKey = escrowKey.holdUserLock(address(dAppControl));
+        userData = abi.encodeWithSelector(executionEnvironment.userWrapper.selector, userOp);
+        userData = abi.encodePacked(userData, escrowKey.pack());
+        vm.prank(address(atlas));
+        (status, data) = address(executionEnvironment).call(userData);
+        assertTrue(status);
+        assertEq(abi.decode(abi.decode(data, (bytes)), (uint256)), expectedReturnValue);
+
+        // CallRevert (needsDelegateUser=false)
+        userOp.data = abi.encodeWithSelector(dAppControl.mockOperation.selector, true, 0);
+        escrowKey = escrowKey.holdUserLock(address(dAppControl));
+        userData = abi.encodeWithSelector(executionEnvironment.userWrapper.selector, userOp);
+        userData = abi.encodePacked(userData, escrowKey.pack());
+        vm.prank(address(atlas));
+        vm.expectRevert(bytes("ERR-EC04a CallRevert"));
+        (status,) = address(executionEnvironment).call(userData);
+        assertTrue(status, "expectRevert ERR-EC04a CallRevert: call did not revert");
+
+        // Change of config
+        callConfig.delegateUser = true;
+        setupDAppControl(callConfig);
+        userOp.dapp = address(dAppControl);
+
+        // Valid (needsDelegateUser=true)
+        expectedReturnValue = 277;
+        userOp.data = abi.encodeWithSelector(dAppControl.mockOperation.selector, false, expectedReturnValue);
+        escrowKey = escrowKey.holdUserLock(address(dAppControl));
+        userData = abi.encodeWithSelector(executionEnvironment.userWrapper.selector, userOp);
+        userData = abi.encodePacked(userData, escrowKey.pack());
+        vm.prank(address(atlas));
+        (status, data) = address(executionEnvironment).call(userData);
+        assertTrue(status);
+        assertEq(abi.decode(abi.decode(data, (bytes)), (uint256)), expectedReturnValue);
+
+        // DelegateRevert (needsDelegateUser=true)
+        userOp.data = abi.encodeWithSelector(dAppControl.mockOperation.selector, true, 0);
+        escrowKey = escrowKey.holdUserLock(address(dAppControl));
+        userData = abi.encodeWithSelector(executionEnvironment.userWrapper.selector, userOp);
+        userData = abi.encodePacked(userData, escrowKey.pack());
+        vm.prank(address(atlas));
+        vm.expectRevert(bytes("ERR-EC02 DelegateRevert"));
+        (status,) = address(executionEnvironment).call(userData);
+        assertTrue(status, "expectRevert ERR-EC02 DelegateRevert: call did not revert");
+    }
+}
+
+contract MockDAppControl is DAppControl {
+    constructor(
+        address _escrow,
+        address _governance,
+        CallConfig memory _callConfig
+    )
+        DAppControl(_escrow, _governance, _callConfig)
+    { }
+
+    /*//////////////////////////////////////////////////////////////
+                        ATLAS OVERRIDE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _preOpsCall(UserOperation calldata userOp) internal override returns (bytes memory) {
+        if (userOp.data.length > 0) {
+            (bool success, bytes memory data) = address(userOp.dapp).call(userOp.data);
+            require(success, "_preOpsCall reverted");
+            return data;
+        }
+        return new bytes(0);
+    }
+
+    function _allocateValueCall(address, uint256, bytes calldata) internal virtual override { }
+    function getBidFormat(UserOperation calldata) public view virtual override returns (address) { }
+    function getBidValue(SolverOperation calldata) public view virtual override returns (uint256) { }
+
+    /*//////////////////////////////////////////////////////////////
+                            CUSTOM FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function mockOperation(bool shouldRevert, uint256 returnValue) public pure returns (uint256) {
+        require(!shouldRevert, "revert requested");
+        return returnValue;
     }
 }
