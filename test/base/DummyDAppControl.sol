4a77682aace7365ac1b7195e1552621ee4b2de98
jj1980a <j.feral@live.fr>
2024-01-05 11:07:36 +0800

    escrow tests

--- test/base/DummyDAppControl.sol
+++ test/base/DummyDAppControl.sol
@@ -7,7 +7,13 @@ import "../../src/contracts/types/DAppApprovalTypes.sol";
 import "../../src/contracts/types/UserCallTypes.sol";
 import "../../src/contracts/types/SolverCallTypes.sol";
 
+library CallConfigBuilder {
+    function allFalseCallConfig() internal pure returns (CallConfig memory) { }
+}
+
 contract DummyDAppControl is DAppControl {
+    event MEVPaymentSuccess(address bidToken, uint256 bidAmount);
+
     constructor(
         address escrow,
         address governance,
@@ -16,12 +22,79 @@ contract DummyDAppControl is DAppControl {
         DAppControl(escrow, governance, _callConfig)
     { }
 
-    function _preOpsCall(UserOperation calldata) internal virtual override returns (bytes memory) { }
-    function _allocateValueCall(address, uint256, bytes calldata) internal virtual override { }
+    // ****************************************
+    // Atlas overrides
+    // ****************************************
+
+    function _preOpsCall(UserOperation calldata userOp) internal virtual override returns (bytes memory) {
+        if (userOp.data.length == 0) {
+            return new bytes(0);
+        }
+
+        (bool success, bytes memory data) = address(userOp.dapp).call(userOp.data);
+        require(success, "_preOpsCall reverted");
+        return data;
+    }
+
+    function _postOpsCall(bool, bytes calldata data) internal pure virtual override returns (bool) {
+        if (data.length == 0) {
+            return true;
+        }
+
+        (bool shouldRevert) = abi.decode(data, (bool));
+        require(!shouldRevert, "_postSolverCall revert requested");
+        return true;
+    }
+
+    function _preSolverCall(bytes calldata data) internal pure virtual override returns (bool) {
+        if (data.length == 0) {
+            return true;
+        }
+
+        (,, bytes memory dAppReturnData) = abi.decode(data, (address, uint256, bytes));
+        (bool shouldRevert, bool returnValue) = abi.decode(dAppReturnData, (bool, bool));
+        require(!shouldRevert, "_preSolverCall revert requested");
+        return returnValue;
+    }
+
+    function _postSolverCall(bytes calldata data) internal pure virtual override returns (bool) {
+        if (data.length == 0) {
+            return true;
+        }
+
+        (,, bytes memory dAppReturnData) = abi.decode(data, (address, uint256, bytes));
+        (bool shouldRevert, bool returnValue) = abi.decode(dAppReturnData, (bool, bool));
+        require(!shouldRevert, "_postSolverCall revert requested");
+        return returnValue;
+    }
+
+    function _allocateValueCall(
+        address bidToken,
+        uint256 winningAmount,
+        bytes calldata data
+    )
+        internal
+        virtual
+        override
+    {
+        if (data.length == 0) {
+            return;
+        }
+
+        (bool shouldRevert) = abi.decode(data, (bool));
+        require(!shouldRevert, "_allocateValueCall revert requested");
+        emit MEVPaymentSuccess(bidToken, winningAmount);
+    }
+
     function getBidFormat(UserOperation calldata) public view virtual override returns (address) { }
     function getBidValue(SolverOperation calldata) public view virtual override returns (uint256) { }
-}
 
-library CallConfigBuilder {
-    function allFalseCallConfig() internal pure returns (CallConfig memory) { }
+    // ****************************************
+    // Custom functions
+    // ****************************************
+
+    function userOperationCall(bool shouldRevert, uint256 returnValue) public pure returns (uint256) {
+        require(!shouldRevert, "userOperationCall revert requested");
+        return returnValue;
+    }
 }
