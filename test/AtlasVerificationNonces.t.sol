eb566fc53051c3550586cf1b8537f0e2e5eee3ea
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-01-17 16:01:51 +0200

    More nonce system tests

--- test/AtlasVerificationNonces.t.sol
+++ test/AtlasVerificationNonces.t.sol
@@ -24,6 +24,36 @@ contract AtlasVerificationNoncesTest is AtlasVerificationBase {
         assertEq(atlasVerification.getNextNonce(governanceEOA, false), 1, "Gov async next nonce should be 1");
     }
 
+    function testUsingSeqNoncesDoNotChangeNextAsyncNonce() public {
+        defaultAtlasWithCallConfig(defaultCallConfig().withUserNoncesSequenced(true).build());
+        assertEq(atlasVerification.getNextNonce(userEOA, true), 1, "User next seq nonce should be 1");
+        assertEq(atlasVerification.getNextNonce(userEOA, false), 1, "User next async nonce should be 1");
+
+        UserOperation memory userOp = validUserOperation().withNonce(1).build();
+        SolverOperation[] memory solverOps = validSolverOperations(userOp);
+        DAppOperation memory dappOp = validDAppOperation(userOp, solverOps).signAndBuild(address(atlasVerification), governancePK);
+        doValidCalls(AtlasVerificationBase.ValidCallsCall({
+            userOp: userOp, solverOps: solverOps, dAppOp: dappOp, msgValue: 0, msgSender: userEOA, isSimulation: false}
+        ));
+        assertEq(atlasVerification.getNextNonce(userEOA, true), 2, "User next seq nonce should now be 2");
+        assertEq(atlasVerification.getNextNonce(userEOA, false), 1, "User next async nonce should still be 1");
+    }
+
+    function testUsingAsyncNoncesDoNotChangeNextSeqNonce() public {
+        defaultAtlasWithCallConfig(defaultCallConfig().withUserNoncesSequenced(false).build());
+        assertEq(atlasVerification.getNextNonce(userEOA, true), 1, "User next seq nonce should be 1");
+        assertEq(atlasVerification.getNextNonce(userEOA, false), 1, "User next async nonce should be 1");
+
+        UserOperation memory userOp = validUserOperation().withNonce(1).build();
+        SolverOperation[] memory solverOps = validSolverOperations(userOp);
+        DAppOperation memory dappOp = validDAppOperation(userOp, solverOps).signAndBuild(address(atlasVerification), governancePK);
+        doValidCalls(AtlasVerificationBase.ValidCallsCall({
+            userOp: userOp, solverOps: solverOps, dAppOp: dappOp, msgValue: 0, msgSender: userEOA, isSimulation: false}
+        ));
+        assertEq(atlasVerification.getNextNonce(userEOA, true), 1, "User next seq nonce should still be 1");
+        assertEq(atlasVerification.getNextNonce(userEOA, false), 2, "User next async nonce should now be 2");
+    }
+
     function testSameNonceCannotBeUsedTwice_UserSequenced_DAppAsync() public {
         defaultAtlasWithCallConfig(defaultCallConfig().withUserNoncesSequenced(true).withDappNoncesSequenced(false).build());
 
@@ -132,10 +162,43 @@ contract AtlasVerificationNoncesTest is AtlasVerificationBase {
         ), ValidCallsResult.Valid);
     }
 
+    function testAsyncNoncesAreValidEvenIfNotSequential() public {
+        defaultAtlasWithCallConfig(defaultCallConfig().withUserNoncesSequenced(false).build());
+
+        UserOperation memory userOp = validUserOperation().withNonce(1).build();
+        SolverOperation[] memory solverOps = validSolverOperations(userOp);
+        DAppOperation memory dappOp = validDAppOperation(userOp, solverOps).withNonce(1).signAndBuild(address(atlasVerification), governancePK);
+        callAndAssert(ValidCallsCall({
+            userOp: userOp, solverOps: solverOps, dAppOp: dappOp, msgValue: 0, msgSender: userEOA, isSimulation: false}
+        ), ValidCallsResult.Valid);
+
+        // Valid if using the async user nonce
+        userOp = validUserOperation().withNonce(100).build();
+        solverOps = validSolverOperations(userOp);
+        dappOp = validDAppOperation(userOp, solverOps).withNonce(2).signAndBuild(address(atlasVerification), governancePK);
+        callAndAssert(ValidCallsCall({
+            userOp: userOp, solverOps: solverOps, dAppOp: dappOp, isSimulation: false, msgValue: 0, msgSender: userEOA}
+        ), ValidCallsResult.Valid);
+
+        // Valid if using the async user nonce
+        userOp = validUserOperation().withNonce(500).build();
+        solverOps = validSolverOperations(userOp);
+        dappOp = validDAppOperation(userOp, solverOps).withNonce(3).signAndBuild(address(atlasVerification), governancePK);
+        callAndAssert(ValidCallsCall({
+            userOp: userOp, solverOps: solverOps, dAppOp: dappOp, isSimulation: false, msgValue: 0, msgSender: userEOA}
+        ), ValidCallsResult.Valid);
+
+        // Valid if using the async user nonce
+        userOp = validUserOperation().withNonce(2).build();
+        solverOps = validSolverOperations(userOp);
+        dappOp = validDAppOperation(userOp, solverOps).withNonce(4).signAndBuild(address(atlasVerification), governancePK);
+        callAndAssert(ValidCallsCall({
+            userOp: userOp, solverOps: solverOps, dAppOp: dappOp, isSimulation: false, msgValue: 0, msgSender: userEOA}
+        ), ValidCallsResult.Valid);
+    }
+
     function testFirstEightNonces_DAppSequenced() public {
         defaultAtlasWithCallConfig(defaultCallConfig().withDappNoncesSequenced(true).build());
-
-        console.log("next gov nonce: ", atlasVerification.getNextNonce(governanceEOA, true), "\n");
         
         UserOperation memory userOp = validUserOperation().build();
         SolverOperation[] memory solverOps = validSolverOperations(userOp);
@@ -147,13 +210,8 @@ contract AtlasVerificationNoncesTest is AtlasVerificationBase {
 
         // Testing nonces 2 - 8
         for (uint256 i = 2; i < 9; i++) {
-
-            console.log("next gov nonce: ", atlasVerification.getNextNonce(governanceEOA, true));
-            console.log("\nTX ", i, "\n");
-
             assertEq(atlasVerification.getNextNonce(governanceEOA, true), i, "Next nonce not incrementing as expected");
             
-
             userOp = validUserOperation().withNonce(i).build();
             solverOps = validSolverOperations(userOp);
             dappOp = validDAppOperation(userOp, solverOps).withNonce(i).signAndBuild(address(atlasVerification), governancePK);
@@ -163,36 +221,6 @@ contract AtlasVerificationNoncesTest is AtlasVerificationBase {
         }
 
         assertEq(atlasVerification.getNextNonce(governanceEOA, true), 9, "Next nonce should be 9 after 8 seq nonces used");
-    }
-
-    function test_bitmap2UsedAfterBitmap1Fulled_Sequenced() public {}
-
-    function test_bitmap3UsedAfterBitmap2Fulled_Sequenced() public {
-        // Use nonce 1 in bitmap 1
-        // Use nonce 239 in bitmap 1 (last in bitmap)
-        // Next nonce is 240, which should tick over to bitmap 2
-        // Use nonce 279 in bitmap 2 (last in bitmap)
-        // Next nonce is 280, which should tick over to bitmap 3
-    }
-
-    function test_emptyNoncesInUsedBitmapsCanBeUsed() public {
-        // Use nonce 1 in bitmap 1
-        // Use nonce 239 in bitmap 1 (last in bitmap)
-        // Try using nonce 2 in bitmap 1, should work - unused
-        // Next nonce is 240, which should tick over to bitmap 2
-        // Try using nonce 3 in bitmap 1, should work - unused
-    }
-
-    // does this need sequenced and non sequenced versions? - YES
-    function test_usedNoncesCannotBeReused() public {
-        // Test nonce 1 works, uses bitmap 1
-        // Test using nonce 1 again fails
-        // Test nonce 2 works, uses bitmap 1
-        // Test using nonce 2 again fails
-        // Jump to bitmap 2
-        // Test nonce x works, uses bitmap 2
-        // Test using nonce x again fails
-        // Test using nonce 1 in bitmap 1 still fails
     }
    
 }
