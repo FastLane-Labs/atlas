7e6617d84c460d79fbb911ef494f32afc8fe2546
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-01-25 20:48:05 +0200

    Use vm.computeCreateAddress instead of deprecated func

--- test/base/AtlasBaseTest.t.sol
+++ test/base/AtlasBaseTest.t.sol
@@ -68,8 +68,8 @@ contract AtlasBaseTest is Test, TestConstants {
         simulator = new Simulator();
 
         // Computes the addresses at which AtlasVerification will be deployed
-        address expectedAtlasAddr = computeCreateAddress(payee, vm.getNonce(payee) + 1);
-        address expectedAtlasVerificationAddr = computeCreateAddress(payee, vm.getNonce(payee) + 2);
+        address expectedAtlasAddr = vm.computeCreateAddress(payee, vm.getNonce(payee) + 1);
+        address expectedAtlasVerificationAddr = vm.computeCreateAddress(payee, vm.getNonce(payee) + 2);
         bytes32 salt = keccak256(abi.encodePacked(block.chainid, expectedAtlasAddr, "AtlasFactory 1.0"));
         ExecutionEnvironment execEnvTemplate = new ExecutionEnvironment{ salt: salt }(expectedAtlasAddr);
 
