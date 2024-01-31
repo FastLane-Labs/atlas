92d54fa2d971ff531653d15b77dcefa86dcaea10
0x2 <75550506+Code0x2@users.noreply.github.com>
2024-01-30 22:20:06 -0800

    surcharge withdraw test

--- test/Surcharge.t.sol
+++ test/Surcharge.t.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.22;
 
 import "forge-std/Test.sol";
-
 import { BaseTest } from "./base/BaseTest.t.sol";
 import { FastLaneErrorsEvents, AtlasEvents } from "src/contracts/types/Emissions.sol";
+
+using stdStorage for StdStorage;
 
 contract SurchargeTest is BaseTest {
     function testSurchargeRecipient() public {
@@ -32,5 +33,26 @@ contract SurchargeTest is BaseTest {
         emit AtlasEvents.SurchargeRecipientTransferred(address(this));
         atlas.becomeSurchargeRecipient();
         assertEq(atlas.surchargeRecipient(), address(this), "recipient should change to new address");
+    }
+
+    function testSurchargeWithdraw() public {
+        // Check a random cant withdraw
+        vm.expectRevert(FastLaneErrorsEvents.InvalidAccess.selector);
+        atlas.withdrawSurcharge();
+
+        // Set surcharge for withdrawal
+        uint256 surchargeSlot = stdstore.target(address(atlas)).sig("surcharge()").find();
+        vm.store(address(atlas), bytes32(surchargeSlot), bytes32(uint256(1 ether)));
+        assertEq(atlas.surcharge(), 1 ether, "surcharge should be set to 1 ether");
+        deal(address(atlas), 1 ether);
+
+        // Test actual withdrawal
+        vm.startPrank(payee);
+        uint256 startingBalance = address(payee).balance;
+        vm.expectEmit(true, true, true, true);
+        emit AtlasEvents.SurchargeWithdrawn(payee, 1 ether);
+        atlas.withdrawSurcharge();
+        uint256 endingBalance = address(payee).balance;
+        assertEq(endingBalance, startingBalance + 1 ether, "payee's balance should increase by 1 ether");
     }
 }
