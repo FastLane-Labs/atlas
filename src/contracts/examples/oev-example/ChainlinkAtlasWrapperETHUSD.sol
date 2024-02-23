94808511ebfe72df69d8201ab429eaf6f2161a19
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-02-23 14:59:10 +0200

    Improve oracle wrapper contract

--- src/contracts/examples/oev-example/ChainlinkAtlasWrapperETHUSD.sol
+++ src/contracts/examples/oev-example/ChainlinkAtlasWrapperETHUSD.sol
@@ -13,9 +13,10 @@ contract ChainlinkAtlasWrapperETHUSD is Ownable {
     int256 public atlasLatestAnswer;
     uint256 public atlasLatestTimestamp;
 
-    mapping(address submitter => bool trusted) public trustedSubmitters;
+    mapping(address transmitter => bool trusted) public transmitters;
 
-    error SubmitterNotTrusted(address submitter);
+    error TransmitterNotTrusted(address transmitter);
+    error InvalidTransmitData();
 
     constructor(address atlas, address _baseChainlinkAggregator) {
         ATLAS = atlas;
@@ -34,15 +35,68 @@ contract ChainlinkAtlasWrapperETHUSD is Ownable {
     }
 
     // Called by a trusted ExecutionEnvironment during an Atlas metacall
-    function submitAtlasAnswer(int256 answer) external {
-        if (!trustedSubmitters[msg.sender]) revert SubmitterNotTrusted(msg.sender);
+    function transmit(
+        bytes calldata _report,
+        bytes32[] calldata _rs,
+        bytes32[] calldata _ss,
+        bytes32 _rawVs
+    )
+        external
+    {
+        if (!transmitters[msg.sender]) revert TransmitterNotTrusted(msg.sender);
+
+        (bool dataVerified, int256 answer) = _verifyTransmitData(_report, _rs, _ss, _rawVs);
+        if (!dataVerified) revert InvalidTransmitData(); // TODO move to more specific reverts in verify func
+
         atlasLatestAnswer = answer;
         atlasLatestTimestamp = block.timestamp;
     }
 
-    function setSubmitterTrustStatus(address submitter, bool trusted) external onlyOwner {
-        trustedSubmitters[submitter] = trusted;
+    // Verifies
+    function _verifyTransmitData(
+        bytes calldata _report,
+        bytes32[] calldata _rs,
+        bytes32[] calldata _ss,
+        bytes32 _rawVs
+    )
+        internal
+        returns (bool, int256)
+    {
+        // TODO implement based on Chainlink Aggregator transmit function logic
     }
+
+    // Owner can add/remove trusted transmitters
+    function setTransmitterStatus(address transmitter, bool trusted) external onlyOwner {
+        transmitters[transmitter] = trusted;
+    }
+}
+
+// -----------------------------------------------
+// Structs and interface for Chainlink Aggregator
+// -----------------------------------------------
+
+struct ReportData {
+    HotVars hotVars; // Only read from storage once
+    bytes observers; // ith element is the index of the ith observer
+    int192[] observations; // ith element is the ith observation
+    bytes vs; // jth element is the v component of the jth signature
+    bytes32 rawReportContext;
+}
+
+struct HotVars {
+    // Provides 128 bits of security against 2nd pre-image attacks, but only
+    // 64 bits against collisions. This is acceptable, since a malicious owner has
+    // easier way of messing up the protocol than to find hash collisions.
+    bytes16 latestConfigDigest;
+    uint40 latestEpochAndRound; // 32 most sig bits for epoch, 8 least sig bits for round
+    // Current bound assumed on number of faulty/dishonest oracles participating
+    // in the protocol, this value is referred to as f in the design
+    uint8 threshold;
+    // Chainlink Aggregators expose a roundId to consumers. The offchain reporting
+    // protocol does not use this id anywhere. We increment it whenever a new
+    // transmission is made to provide callers with contiguous ids for successive
+    // reports.
+    uint32 latestAggregatorRoundId;
 }
 
 interface IChainlinkAggregator {
