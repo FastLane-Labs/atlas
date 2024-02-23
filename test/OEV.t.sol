86db16dd261ab0295f1ad228f897148cbd0261c4
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-02-23 13:57:26 +0200

    Add test transmit calldata helper

--- test/OEV.t.sol
+++ test/OEV.t.sol
@@ -16,7 +16,12 @@ import { ChainlinkDAppControl } from "src/contracts/examples/oev-example/Chainli
 import { SolverBase } from "src/contracts/solver/SolverBase.sol";
 
 
-contract SwapIntentTest is BaseTest {
+// Using this Chainlink update to ETHUSD feed as an example:
+// Aggregator: https://etherscan.io/address/0xE62B71cf983019BFf55bC83B48601ce8419650CC
+// Transmit tx: https://etherscan.io/tx/0x3645d1bc223efe0861e02aeb95d6204c5ebfe268b64a7d23d385520faf452bc0
+// ETH/USD set to: $2941.02 == 294102000000
+
+contract OEVTest is BaseTest {
     ChainlinkDAppControl public chainlinkDAppControl;
     TxBuilder public txBuilder;
     Sig public sig;
@@ -26,6 +31,39 @@ contract SwapIntentTest is BaseTest {
         bytes32 r;
         bytes32 s;
     }
+
+    struct TransmitPayload {
+        bytes reportBytes;
+        bytes32[] rs;
+        bytes32[] ss;
+        bytes32 rawVs;
+    }
+
+    struct ReportData {
+        HotVars hotVars; // Only read from storage once
+        bytes observers; // ith element is the index of the ith observer
+        int192[] observations; // ith element is the ith observation
+        bytes vs; // jth element is the v component of the jth signature
+        bytes32 rawReportContext;
+    }
+
+    struct HotVars {
+        // Provides 128 bits of security against 2nd pre-image attacks, but only
+        // 64 bits against collisions. This is acceptable, since a malicious owner has
+        // easier way of messing up the protocol than to find hash collisions.
+        bytes16 latestConfigDigest;
+        uint40 latestEpochAndRound; // 32 most sig bits for epoch, 8 least sig bits for round
+        // Current bound assumed on number of faulty/dishonest oracles participating
+        // in the protocol, this value is referred to as f in the design
+        uint8 threshold;
+        // Chainlink Aggregators expose a roundId to consumers. The offchain reporting
+        // protocol does not use this id anywhere. We increment it whenever a new
+        // transmission is made to provide callers with contiguous ids for successive
+        // reports.
+        uint32 latestAggregatorRoundId;
+    }
+
+    
 
     function setUp() public virtual override {
         BaseTest.setUp();
@@ -57,6 +95,54 @@ contract SwapIntentTest is BaseTest {
         // 2. solverOps - capture OEV by liquidating things that use the oracle wrapper
         // 3. postOpsCall - update the base chainlink oracle with signed `transmit` data
         
+    }
+
+
+    // ----------------
+    // OEV Test Utils
+    // ----------------
+
+    // Returns calldata taken from a real Chainlink ETH/USD transmit tx
+    function getTransmitPayload() public returns (TransmitPayload memory) {
+        // From this ETHUSD transmit tx: Transmit tx:
+        // https://etherscan.io/tx/0x3645d1bc223efe0861e02aeb95d6204c5ebfe268b64a7d23d385520faf452bc0
+        // ETHUSD set to: $2941.02 == 294102000000
+
+        bytes memory report = hex"000000000000000000000047ddec946856fa8055ac2202f633de330001769d050a1718161a110212090c1d1b191c0b0e030001140f131e1508060d04100705000000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000001f00000000000000000000000000000000000000000000000000000044759deacc0000000000000000000000000000000000000000000000000000004475d8ca020000000000000000000000000000000000000000000000000000004475d8ca020000000000000000000000000000000000000000000000000000004475d8ca100000000000000000000000000000000000000000000000000000004476517782000000000000000000000000000000000000000000000000000000447664840f0000000000000000000000000000000000000000000000000000004476a015190000000000000000000000000000000000000000000000000000004476a015190000000000000000000000000000000000000000000000000000004476a01519000000000000000000000000000000000000000000000000000000447779d953000000000000000000000000000000000000000000000000000000447914dd8f000000000000000000000000000000000000000000000000000000447914dd8f000000000000000000000000000000000000000000000000000000447914dd8f000000000000000000000000000000000000000000000000000000447914dd8f0000000000000000000000000000000000000000000000000000004479d861800000000000000000000000000000000000000000000000000000004479d861800000000000000000000000000000000000000000000000000000004479d861800000000000000000000000000000000000000000000000000000004479d861800000000000000000000000000000000000000000000000000000004479d861800000000000000000000000000000000000000000000000000000004479d9ce27000000000000000000000000000000000000000000000000000000447a9ebec0000000000000000000000000000000000000000000000000000000447a9ebec0000000000000000000000000000000000000000000000000000000447ad9a8c8000000000000000000000000000000000000000000000000000000447b281300000000000000000000000000000000000000000000000000000000447b281300000000000000000000000000000000000000000000000000000000447b281300000000000000000000000000000000000000000000000000000000447b3df490000000000000000000000000000000000000000000000000000000447b3df490000000000000000000000000000000000000000000000000000000447b5d3856000000000000000000000000000000000000000000000000000000447b5d3856000000000000000000000000000000000000000000000000000000447b5d3856";
+        bytes32[] memory rs = new bytes32[](10);
+        bytes32[] memory ss = new bytes32[](10);
+        bytes32 rawVs = 0x0100010101000000000001000000000000000000000000000000000000000000;
+
+        rs[0] = 0x5bde6c6a1b27f1421400fba9850abcd4d9701d7178d2df1d469c693a9ab6389e;
+        rs[1] = 0xf9518ba45aaf8fc019962dee3144087723bcac49ce1b3d12d075abc030ae51f1;
+        rs[2] = 0x657ae99064988794f99b98dfdd6ebc9f5c0967f75e5ce61392d81d78e8434e0a;
+        rs[3] = 0xfc4ef6fa4d47c3a8cb247799831c4db601387106cc9f80ef710fec5d06b07c53;
+        rs[4] = 0x813516330ff60244a90a062f4ddcb18611711ed217cf146041439e26a6c1d687;
+        rs[5] = 0x8aa23424c2fdd1d2f459fc901c3b21c611d17c07b0df51c48f17bd6bcc5d8c54;
+        rs[6] = 0xe40ea4755faebccfccbf9ca25bd518427877c9155526db04458b9753034ad552;
+        rs[7] = 0x44fbb6b9ab6f56f29d5d1943fa6d6b13c993e213ba3b20f6a5d20224cb3f942d;
+        rs[8] = 0xe2a4e3529c077a128bc52d5e1b11cf64bc922100bafe6ebc95654fea49a5d355;
+        rs[9] = 0x4588680888b56cda77a1b49b32807ba33e7009a182a048d33496d70987aebcbc;
+        rs[10] = 0x7ad51d2aa5e792f46ac17784a3a362f0fff3dc7f805ef8f74324113d8b475249;
+
+        ss[0] = 0x3eb07f321322b7d0ea0c90ca48af408e9b6daaaf0e33993436155ef83d1e7d0e;
+        ss[1] = 0x5ff05d281bf7c1db924036e0123765adfef8b4f563d981da9c7011dc3b1e6c79;
+        ss[2] = 0x7fdb65f4084636a904129a327d99a8ef5cdcadc3e6e266626823eb1adab4532d;
+        ss[3] = 0x07025b9483f5ad5ee55f07b6cddbabc1411d9570ced7d11a3d504cf38853e8a3;
+        ss[4] = 0x332a4b577c831d9dae5ea3eb4ee5832cdd6672c4bd7c97e5fb2dae3b4b99d02f;
+        ss[5] = 0x45b4181c3b95f15fc40a40fb74344d1ef45c33dfbe99587237a1a4c875aae024;
+        ss[6] = 0x2a2eb5e729343c2f093c6b6ec71f0b1eb07bf133ead806976672dcbf90abcaca;
+        ss[7] = 0x54ca9bd4122a43f119d3d24072db5be9479f9271d32f332e76ff3a70feeb7fd3;
+        ss[8] = 0x1a7aaeda65f1cabb53a43f080ab8d76337107b0e7bf013096eecdefcf88af56a;
+        ss[9] = 0x7a1e1d7b9c865b34d966a94e21f0d2c73473a30bf03c1f8eff7b88b5c3183c31;
+        ss[10] = 0x2768d43bca650e9a452db41730c4e31b600f5398c49490d66f4065a0b357707f;
+
+        return TransmitPayload({
+            reportBytes: report,
+            rs: rs,
+            ss: ss,
+            rawVs: rawVs
+        });
     }
 }
 
