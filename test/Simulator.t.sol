bdf7283857eb665cef327638a8ebb9b0c461c137
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-03-12 22:36:38 +0200

    Add tests for simSolverCall

--- test/Simulator.t.sol
+++ test/Simulator.t.sol
@@ -9,6 +9,8 @@ import { SolverOperation } from "src/contracts/types/SolverCallTypes.sol";
 import { AtlasEvents } from "src/contracts/types/AtlasEvents.sol";
 import { AtlasErrors } from "src/contracts/types/AtlasErrors.sol";
 import { ValidCallsResult } from "src/contracts/types/ValidCallsTypes.sol";
+import { SolverOutcome } from "src/contracts/types/EscrowTypes.sol";
+import { CallVerification } from "src/contracts/libraries/CallVerification.sol";
 
 import { BaseTest } from "./base/BaseTest.t.sol";
 import { UserOperationBuilder } from "./base/builders/UserOperationBuilder.sol";
@@ -18,6 +20,7 @@ import { CallConfigBuilder } from "./helpers/CallConfigBuilder.sol";
 import { DummyDAppControlBuilder } from "./helpers/DummyDAppControlBuilder.sol";
 
 import { DummyDAppControl } from "./base/DummyDAppControl.sol";
+import { SolverBase } from "src/contracts/solver/SolverBase.sol";
 
 
 contract SimulatorTest is BaseTest {
@@ -32,6 +35,74 @@ contract SimulatorTest is BaseTest {
         address msgSender;
         bool isSimulation;
     }
+
+    function setUp() public override {
+        BaseTest.setUp();
+        dAppControl = defaultDAppControl().buildAndIntegrate(atlasVerification);
+    }
+
+    function test_simUserOperation_success_valid() public {
+        UserOperation memory userOp = validUserOperation().build();
+
+        (bool success, uint256 validCallsResult) = simulator.simUserOperation(userOp);
+
+        assertEq(success, true);
+        assertEq(validCallsResult, 0);
+    }
+
+    function test_simUserOperation_fail_bubblesUpValidCallsResult() public {
+        UserOperation memory userOp = validUserOperation().build();
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPK, "wrong data");
+        userOp.signature = abi.encodePacked(r, s, v); // use bad sig
+
+        (bool success, uint256 validCallsResult) = simulator.simUserOperation(userOp);
+
+        assertEq(success, false);
+        assertEq(validCallsResult, uint256(ValidCallsResult.UserSignatureInvalid));
+    }
+
+    function test_simSolverCall_success_validSolverOutcome() public {
+        vm.startPrank(solverOneEOA);
+        DummySolver solver = new DummySolver(WETH_ADDRESS, address(atlas));
+        atlas.bond(1e18);
+        vm.stopPrank();
+
+        UserOperation memory userOp = validUserOperation().build();
+        SolverOperation[] memory solverOps = new SolverOperation[](1);
+        solverOps[0] = validSolverOperation(userOp).withSolver(address(solver)).withData(abi.encodeWithSelector(solver.solverFunc.selector)).signAndBuild(address(atlasVerification), solverOnePK);
+        DAppOperation memory dAppOp = validDAppOperation(userOp, solverOps).build();
+
+        (bool success, uint256 solverOutcomeResult) = simulator.simSolverCall(userOp, solverOps[0], dAppOp);
+
+        assertEq(success, true);
+        assertEq(solverOutcomeResult, 0);
+    }
+
+    function test_simSolverCall_fail_bubblesUpSolverOutcomeResult() public {
+        vm.startPrank(solverOneEOA);
+        DummySolver solver = new DummySolver(WETH_ADDRESS, address(atlas));
+        // atlas.bond(1e18); - DO NOT BOND - Triggers InsufficientEscrow error
+        vm.stopPrank();
+
+        UserOperation memory userOp = validUserOperation().build();
+        SolverOperation[] memory solverOps = new SolverOperation[](1);
+        solverOps[0] = validSolverOperation(userOp).withSolver(address(solver)).withData(abi.encodeWithSelector(solver.solverFunc.selector)).signAndBuild(address(atlasVerification), solverOnePK);
+        DAppOperation memory dAppOp = validDAppOperation(userOp, solverOps).build();
+
+        (bool success, uint256 solverOutcomeResult) = simulator.simSolverCall(userOp, solverOps[0], dAppOp);
+
+        assertEq(success, false);
+        assertEq(solverOutcomeResult, 1 << uint256(SolverOutcome.InsufficientEscrow));
+    }
+
+    function test_simSolverCalls_success_validSolverOutcome() public {}
+
+    function test_simSolverCalls_fail_noSolverOps() public {}
+
+    function test_simSolverCalls_fail_bubblesUpSolverOutcomeResult() public {}
+
+
+    // Test Helpers
 
     function defaultCallConfig() public returns (CallConfigBuilder) {
         return new CallConfigBuilder();
@@ -59,47 +130,63 @@ contract SimulatorTest is BaseTest {
             .sign(address(atlasVerification), userPK);
     }
 
-    function setUp() public override {
-        BaseTest.setUp();
-        dAppControl = defaultDAppControl().buildAndIntegrate(atlasVerification);
+    function validSolverOperation(UserOperation memory userOp) public returns (SolverOperationBuilder) {
+        return new SolverOperationBuilder()
+            .withFrom(solverOneEOA)
+            .withTo(address(atlas))
+            .withValue(0)
+            .withGas(1_000_000)
+            .withMaxFeePerGas(userOp.maxFeePerGas)
+            .withDeadline(userOp.deadline)
+            .withControl(userOp.control)
+            .withData("")
+            .withUserOpHash(userOp)
+            .sign(address(atlasVerification), solverOnePK);
     }
 
-    function test_sim_temp() public {
-        
-        // Fail in VERIFICATION.validateCalls with userSignatureInvalid
-
-        UserOperation memory userOp = validUserOperation().build();
-
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPK, "wrong data");
-        userOp.signature = abi.encodePacked(r, s, v);
-
-        simulator.simUserOperation(userOp);
+    function validSolverOperations(UserOperation memory userOp) public returns (SolverOperation[] memory) {
+        SolverOperation[] memory solverOps = new SolverOperation[](1);
+        solverOps[0] = validSolverOperation(userOp).build();
+        return solverOps;
     }
 
-    function test_simUserOperation_success_valid() public {
-        UserOperation memory userOp = validUserOperation().build();
-        (bool success, uint256 validCallsResult) = simulator.simUserOperation(userOp);
-        assertEq(success, true);
-        assertEq(validCallsResult, 0);
+    function validDAppOperation(DAppConfig memory config, UserOperation memory userOp, SolverOperation[] memory solverOps) public returns (DAppOperationBuilder) {
+        bytes32 callChainHash = CallVerification.getCallChainHash(config, userOp, solverOps);
+        return new DAppOperationBuilder()
+            .withFrom(governanceEOA)
+            .withTo(address(atlas))
+            .withValue(0)
+            .withGas(2_000_000)
+            .withNonce(address(atlasVerification), governanceEOA)
+            .withDeadline(userOp.deadline)
+            .withControl(userOp.control)
+            .withBundler(address(0))
+            .withUserOpHash(userOp)
+            .withCallChainHash(callChainHash)
+            .sign(address(atlasVerification), governancePK);
     }
 
-    function test_simUserOperation_fail_bubblesUpValidCallsResult() public {
-        UserOperation memory userOp = validUserOperation().build();
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPK, "wrong data");
-        userOp.signature = abi.encodePacked(r, s, v); // use bad sig
-        (bool success, uint256 validCallsResult) = simulator.simUserOperation(userOp);
-        assertEq(success, false);
-        assertEq(validCallsResult, uint256(ValidCallsResult.UserSignatureInvalid));
+    function validDAppOperation(UserOperation memory userOp, SolverOperation[] memory solverOps) public returns (DAppOperationBuilder) {
+        return new DAppOperationBuilder()
+            .withFrom(governanceEOA)
+            .withTo(address(atlas))
+            .withValue(0)
+            .withGas(2_000_000)
+            .withNonce(address(atlasVerification), governanceEOA)
+            .withDeadline(userOp.deadline)
+            .withControl(userOp.control)
+            .withBundler(address(0))
+            .withUserOpHash(userOp)
+            .withCallChainHash(userOp, solverOps)
+            .sign(address(atlasVerification), governancePK);
     }
 
-    function test_simSolverCall_success_validSolverOutcome() public {}
+}
 
-    function test_simSolverCall_fail_bubblesUpSolverOutcomeResult() public {}
 
-    function test_simSolverCalls_success_validSolverOutcome() public {}
-
-    function test_simSolverCalls_fail_noSolverOps() public {}
-
-    function test_simSolverCalls_fail_bubblesUpSolverOutcomeResult() public {}
-
+contract DummySolver is SolverBase {
+    constructor(address weth, address atlas) SolverBase(weth, atlas, msg.sender) { }
+    function solverFunc() public { }
+    fallback() external payable { }
+    receive() external payable { }
 }
