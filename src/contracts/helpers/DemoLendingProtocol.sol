790e8d5bd6943579602cff9c0907286e9e557c96
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-05-02 17:33:56 +0200

    feat: add liquidate, deposit, withdraw fns to DemoLendingProtocol

--- src/contracts/helpers/DemoLendingProtocol.sol
+++ src/contracts/helpers/DemoLendingProtocol.sol
@@ -1,43 +1,107 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
+import { SafeERC20, IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-// A super basic Mock Lending Protocol to demo OEV captured through liquidations with Atlas.
-// Liquidatable positions are created
+// A super basic Demo Lending Protocol to illustrate OEV captured through liquidations with Atlas.
+//
+// In a real lending protocol, collateral is deposited and borrowed against. If the "health factor" (ratio of borrowed
+// value to collateral value) falls below a certain threshold, the position is liquidatable. Liquidations are performed
+// by a liquidator returning the borrowed asset in exchange for the collateral + a liquidation fee.
+//
+// In this demo protocol, collateral is deposited and a liquidation price is set, per account. If the price reported by
+// the oracle is below the liquidation price, the account can be liquidated simply by calling `liquidate()` - the
+// liquidator does not need to return any assets, and will receive the entire amount deposited by the targeted account.
+//
+// These simplifications keep the focus on OEV - Oracle Extractable Value.
+
+struct Position {
+    uint256 amount; // amount of deposit token in the position
+    uint256 liquidationPrice; // price at which the position can be liquidated
+}
+
 contract DemoLendingProtocol is Ownable {
-    address public immutable DEPOSIT_TOKEN;
-    address public oracle;
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable DEPOSIT_TOKEN;
+    IChainlinkFeed public chainlinkFeed;
+
+    mapping(address account => Position) public positions;
+
+    error PositionNotLiquidatable();
+    error PositionNotFound();
+    error PositionAlreadyExists();
+
+    event Deposit(address indexed account, uint256 amount, uint256 liquidationPrice);
+    event Withdraw(address indexed account, uint256 amount);
+    event Liquidation(address indexed account, address indexed recipient, uint256 amount);
 
     constructor(address depositToken) Ownable() {
-        DEPOSIT_TOKEN = depositToken;
+        DEPOSIT_TOKEN = IERC20(depositToken);
     }
 
     // ---------------------------------------------------- //
-    //                     User Functions                   //
+    //                    Setup Functions                   //
     // ---------------------------------------------------- //
 
+    // Deposits `amount` of deposit token into caller's position, setting the liquidation price to `liquidationPrice`.
     function deposit(uint256 amount, uint256 liquidationPrice) external {
-        // Deposit funds into the lending protocol
+        Position memory position = positions[msg.sender];
+
+        if (position.amount > 0) revert PositionAlreadyExists();
+
+        DEPOSIT_TOKEN.safeTransferFrom(msg.sender, address(this), amount);
+        positions[msg.sender] = Position(amount, liquidationPrice);
+
+        emit Deposit(msg.sender, amount, liquidationPrice);
     }
 
+    // Withdraws funds in caller's position if not liquidated, and deletes the position.
     function withdraw() external {
-        // Withdraw funds from the lending protocol
+        Position memory position = positions[msg.sender];
+
+        if (position.amount == 0) revert PositionNotFound();
+
+        DEPOSIT_TOKEN.safeTransfer(msg.sender, position.amount);
+        delete positions[msg.sender];
+
+        emit Withdraw(msg.sender, position.amount);
     }
 
     // ---------------------------------------------------- //
     //                    Solver Functions                  //
     // ---------------------------------------------------- //
 
-    function liquidate(address user) external {
-        // Liquidate a user's position
+    // Called by a solver. If targeted account's position is liquidatable, the position amount is sent to the
+    // `recipient` address. If position is not liquidatable, reverts.
+    function liquidate(address account, address recipient) external {
+        Position memory position = positions[account];
+
+        if (position.amount == 0) revert PositionNotFound();
+        if (position.liquidationPrice < uint256(chainlinkFeed.latestAnswer())) revert PositionNotLiquidatable();
+
+        // Send liquidated amount to solver's recipient, and delete the account's position
+        DEPOSIT_TOKEN.safeTransfer(recipient, position.amount);
+        delete positions[account];
+
+        emit Liquidation(account, recipient, position.amount);
     }
 
     // ---------------------------------------------------- //
     //                     Owner Functions                  //
     // ---------------------------------------------------- //
 
-    function setOracle(address newOracle) external onlyOwner {
-        oracle = newOracle;
+    function setOracle(address newChainlinkFeed) external onlyOwner {
+        chainlinkFeed = IChainlinkFeed(newChainlinkFeed);
     }
+}
+
+interface IChainlinkFeed {
+    function latestAnswer() external view returns (int256);
+    function latestTimestamp() external view returns (uint256);
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 }
