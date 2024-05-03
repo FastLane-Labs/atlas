116101e404744c4b26e2e80f533c95cb23b65266
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-05-03 19:21:48 +0200

    WIP: Added Chainlink ETH/USD Sepolia Signers to OEV demo deploy script

--- script/deploy-oev-demo.s.sol
+++ script/deploy-oev-demo.s.sol
@@ -34,6 +34,31 @@ contract DeployOEVDemoScript is DeployBaseScript {
 
         // TODO
 
+        atlas = Atlas(payable(_getAddressFromDeploymentsJson("ATLAS")));
+        // atlas = Atlas(payable(0xa892eb9F79E0D1b6277B3456b0a8FE770386f6DB)); OLD
+        atlasVerification = AtlasVerification(payable(_getAddressFromDeploymentsJson("ATLAS_VERIFICATION")));
+        // atlasVerification = AtlasVerification(payable(0xeeB91b2d317e3A747E88c1CA542ae31E32B87FDF));
+
+        // Deploy token used in Demo Lending Protocol
+        dai = new Token("DAI Stablecoin", "DAI", 18);
+
+        // Deploy Demo Lending Protocol
+        lendingProtocol = new DemoLendingProtocol(address(dai));
+
+        // Deploy and initialize Chainlink DAppControl
+        chainlinkDAppControl = new ChainlinkDAppControl(address(atlas));
+        atlasVerification.initializeGovernance(address(chainlinkDAppControl));
+
+        // TODO - done by DApp Gov. Need signers array still
+        // Set Chainlink ETH/USD signers in DAppControl
+        // chainlinkDAppControl.setSignersForBaseFeed(chainlinkETHUSD, getETHUSDSigners_Sepolia());
+
+        // TODO - done by Chainlink Node Operator
+        // vm.broadcast(chainlinkNodePrivateKey);
+        // address executionEnvironment = atlas.createExecutionEnvironment(address(chainlinkDAppControl));
+
+        // TODO come back to this
+
         vm.stopBroadcast();
 
         console.log("\n");
@@ -46,5 +71,14 @@ contract DeployOEVDemoScript is DeployBaseScript {
 
     function createLiquidatablePosition(uint256 amount, uint256 liquidationPrice) public {
         // TODO approve DAI, deposit into DemoLendingProtocol
+    }
+
+    function getETHUSDSigners_Sepolia() public view returns (address[] memory) {
+        address[] memory signers = new address[](4);
+        signers[0] = 0xb4DC896090D778acC910db4D31f23d3667Add7Db;
+        signers[1] = 0x1b178090f318c9Fd2322D52a5aC85ebBcE6Bf5E7;
+        signers[2] = 0xB69dC0CaD9c739220A941f3D1C013ffd4CE7Dd6C;
+        signers[3] = 0x8CACd95416d74702bccC9336Fc15F7dD0b533dDe;
+        return signers;
     }
 }
