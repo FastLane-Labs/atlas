a530f81bc5b83c4957e8078dd1500640f4d6eed2
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-05-04 12:13:28 +0200

    feat: finished deploy script for OEV demo

--- script/deploy-oev-demo.s.sol
+++ script/deploy-oev-demo.s.sol
@@ -17,27 +17,74 @@ import { DemoLendingProtocol } from "src/contracts/helpers/DemoLendingProtocol.s
 contract DeployOEVDemoScript is DeployBaseScript {
     address public constant CHAINLINK_ETH_USD = 0x694AA1769357215DE4FAC081bf1f309aDC325306; // on Sepolia
 
+    // To be deployed by Chainlink Gov
+    ChainlinkDAppControl chainlinkDAppControl;
+    address chainlinkExecutionEnv;
+
+    // To be deployed by Lending Gov
     Token dai;
     DemoLendingProtocol lendingProtocol;
-    ChainlinkDAppControl chainlinkDAppControl;
     ChainlinkAtlasWrapper lendingProtocolChainlinkWrapper;
 
     function run() external {
         console.log("\n=== DEPLOY OEV DEMO ===\n");
 
-        uint256 deployerPrivateKey = vm.envUint("DAPP_GOV_PRIVATE_KEY");
-        address deployer = vm.addr(deployerPrivateKey);
+        // Chainlink Gov actions:
+        // - Deploy Chainlink DAppControl and initialize with Atlas
+        // - Creates Execution Environment for Atlas OEV Metacalls
+        uint256 chainlinkGovPrivateKey = vm.envUint("DAPP_GOV_PRIVATE_KEY");
+        address chainlinkGov = vm.addr(chainlinkGovPrivateKey);
 
-        console.log("Deployer address: \t\t\t\t", deployer);
+        // Lending Gov actions:
+        // - Deploy DAI (acts as liquidatable collateral in Lending Protocol)
+        // - Deploy Demo Lending Protocol
+        // - Creates ChainlinkAtlasWrapper for ETH/USD
+        // - Set ChainlinkAtlasWrapper as oracle in Demo Lending Protocol
+        // - Set Chainlink Node's Execution Environment as transmitter in ChainlinkAtlasWrapper
+        uint256 lendingGovPrivateKey = vm.envUint("LENDING_GOV_PRIVATE_KEY");
+        address lendingGov = vm.addr(lendingGovPrivateKey);
 
-        vm.startBroadcast(deployerPrivateKey);
+        console.log("Chainlink Gov address: \t\t\t", chainlinkGov);
+        console.log("Lending Gov address: \t\t\t\t", lendingGov);
+        console.log("\n");
 
-        // TODO
-
+        // Atlas and AtlasVerification instances from latest deployment addresses
         atlas = Atlas(payable(_getAddressFromDeploymentsJson("ATLAS")));
-        // atlas = Atlas(payable(0xa892eb9F79E0D1b6277B3456b0a8FE770386f6DB)); OLD
         atlasVerification = AtlasVerification(payable(_getAddressFromDeploymentsJson("ATLAS_VERIFICATION")));
-        // atlasVerification = AtlasVerification(payable(0xeeB91b2d317e3A747E88c1CA542ae31E32B87FDF));
+
+        console.log("Using Atlas deployed at: \t\t\t", address(atlas));
+        console.log("Using AtlasVerification deployed at: \t\t", address(atlasVerification));
+        console.log("\n");
+
+        // ---------------------------------------------------- //
+        //                   Chainlink Gov Txs                  //
+        // ---------------------------------------------------- //
+
+        vm.startBroadcast(chainlinkGovPrivateKey);
+        console.log("Deploying from Chainlink Gov Account...");
+
+        // Deploy and initialize Chainlink DAppControl with Atlas
+        chainlinkDAppControl = new ChainlinkDAppControl(address(atlas));
+        atlasVerification.initializeGovernance(address(chainlinkDAppControl));
+
+        // Set Chainlink ETH/USD signers in DAppControl
+        chainlinkDAppControl.setSignersForBaseFeed(CHAINLINK_ETH_USD, getETHUSDSigners_Sepolia());
+
+        // Create Execution Environment for Chainlink OEV Metacalls
+        chainlinkExecutionEnv = atlas.createExecutionEnvironment(address(chainlinkDAppControl));
+
+        vm.stopBroadcast();
+        console.log("Contracts deployed by Chainlink Gov:");
+        console.log("Chainlink DAppControl: \t\t\t", address(chainlinkDAppControl));
+        console.log("Chainlink Execution Environment: \t\t", chainlinkExecutionEnv);
+        console.log("\n");
+
+        // ---------------------------------------------------- //
+        //                    Lending Gov Txs                   //
+        // ---------------------------------------------------- //
+
+        vm.startBroadcast(lendingGovPrivateKey);
+        console.log("Deploying from Lending Gov Account...");
 
         // Deploy token used in Demo Lending Protocol
         dai = new Token("DAI Stablecoin", "DAI", 18);
@@ -45,34 +92,52 @@ contract DeployOEVDemoScript is DeployBaseScript {
         // Deploy Demo Lending Protocol
         lendingProtocol = new DemoLendingProtocol(address(dai));
 
-        // Deploy and initialize Chainlink DAppControl
-        chainlinkDAppControl = new ChainlinkDAppControl(address(atlas));
-        atlasVerification.initializeGovernance(address(chainlinkDAppControl));
+        // Create a new ChainlinkAtlasWrapper for ETH/USD for the Demo Lending Protocol
+        lendingProtocolChainlinkWrapper =
+            ChainlinkAtlasWrapper(payable(chainlinkDAppControl.createNewChainlinkAtlasWrapper(CHAINLINK_ETH_USD)));
 
-        // TODO - done by DApp Gov. Need signers array still
-        // Set Chainlink ETH/USD signers in DAppControl
-        // chainlinkDAppControl.setSignersForBaseFeed(chainlinkETHUSD, getETHUSDSigners_Sepolia());
+        // Set the ChainlinkAtlasWrapper as the oracle of the Demo Lending Protocol
+        lendingProtocol.setOracle(address(lendingProtocolChainlinkWrapper));
 
-        // TODO - done by Chainlink Node Operator
-        // vm.broadcast(chainlinkNodePrivateKey);
-        // address executionEnvironment = atlas.createExecutionEnvironment(address(chainlinkDAppControl));
-
-        // TODO come back to this
-
+        // Set Chainlink Gov's Execution Environment as transmitter in Lending Protocol's ChainlinkAtlasWrapper
+        lendingProtocolChainlinkWrapper.setTransmitterStatus(chainlinkExecutionEnv, true);
+        
         vm.stopBroadcast();
 
-        console.log("\n");
-        console.log("Demo Lending Protocol: \t\t", address(lendingProtocol));
-        console.log("Chainlink DAppControl: \t\t", address(chainlinkDAppControl));
-        console.log("Protocol's Chainlink Wrapper: \t\t\t", address(lendingProtocolChainlinkWrapper));
-        console.log("Real Chainlink Feed: \t\t\t", CHAINLINK_ETH_USD);
+        // EXTRA STEP: Lending Gov creates liquidatable position in Demo Lending Protocol
+        createLiquidatablePosition(lendingGovPrivateKey, lendingGovPrivateKey, 100e18, 3000e8);
+        
+        console.log("Contracts deployed by Lending Gov:");
+        console.log("DAI Token: \t\t\t\t\t", address(dai));
+        console.log("Demo Lending Protocol: \t\t\t", address(lendingProtocol));
+        console.log("ChainlinkAtlasWrapper for ETH/USD: \t\t", address(lendingProtocolChainlinkWrapper));
         console.log("\n");
     }
 
-    function createLiquidatablePosition(uint256 amount, uint256 liquidationPrice) public {
-        // TODO approve DAI, deposit into DemoLendingProtocol
+    // NOTE: `liquidationPrice` is specified with 8 decimals, as this is the price format reported by Chainlink oracles.
+    // e.g. $5 = 500000000.
+    function createLiquidatablePosition(
+        uint256 lendingGovPK,
+        uint256 depositorPK,
+        uint256 amount,
+        uint256 liquidationPrice
+    )
+        public
+    {
+        // Lending Gov mints DAI to depositor
+        vm.startBroadcast(lendingGovPK);
+        dai.mint(vm.addr(depositorPK), amount);
+        vm.stopBroadcast();
+
+        // Depositor deposits DAI into Demo Lending Protocol, sets liquidationPrice
+        vm.startBroadcast(depositorPK);
+        dai.approve(address(lendingProtocol), amount);
+        lendingProtocol.deposit(amount, liquidationPrice);
+        vm.stopBroadcast();
     }
 
+    // Chainlink ETH/USD signers on Sepolia
+    // Use https://github.com/BenSparksCode/chainlink-signer-reader to get signer array of any Chainlink Aggregator
     function getETHUSDSigners_Sepolia() public view returns (address[] memory) {
         address[] memory signers = new address[](4);
         signers[0] = 0xb4DC896090D778acC910db4D31f23d3667Add7Db;
