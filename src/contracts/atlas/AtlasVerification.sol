8f0b344d68490ed905b313589d421f70fbc5abb9
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-05-22 11:27:41 +0100

    fix: add `callConfig` to `USER_TYPEHASH` and `_getUserOpHash`

--- src/contracts/atlas/AtlasVerification.sol
+++ src/contracts/atlas/AtlasVerification.sol
@@ -630,6 +630,7 @@ contract AtlasVerification is EIP712, DAppIntegration {
                 userOp.deadline,
                 userOp.dapp,
                 userOp.control,
+                userOp.callConfig,
                 userOp.sessionKey,
                 keccak256(userOp.data)
             )
