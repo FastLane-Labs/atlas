196b999e5d9b6dbec2bf09b8ccd5e82e00e59686
Amitabh Anand <amitabh009@gmail.com>
2024-06-19 18:30:26 +0530

    comments in structs

--- src/contracts/types/UserCallTypes.sol
+++ src/contracts/types/UserCallTypes.sol
@@ -8,15 +8,15 @@ bytes32 constant USER_TYPEHASH = keccak256(
 struct UserOperation {
     address from; // User EOA address
     address to; // Atlas address
-    uint256 value;
-    uint256 gas;
-    uint256 maxFeePerGas;
-    uint256 nonce;
-    uint256 deadline;
-    address dapp; // nested "to" for user's call
-    address control; // address for preOps / validation funcs
-    uint32 callConfig;
-    address sessionKey;
-    bytes data;
-    bytes signature;
+    uint256 value; // Amount of ETH required for the user operation (used in `value` field of the user call)
+    uint256 gas; // Gas limit for the user operation
+    uint256 maxFeePerGas; // Max fee per gas for the user operation
+    uint256 nonce; // Atlas nonce of the user operation available in the AtlasVerification contract
+    uint256 deadline; // block.number deadline for the user operation
+    address dapp; // Nested "to" for user's call (used in `to` field of the user call)
+    address control; // Address of the DAppControl contract
+    uint32 callConfig; // Call configuration expected by user, refer to `src/contracts/types/DAppApprovalTypes.sol:CallConfig`
+    address sessionKey; // Address of the temporary session key which is used to sign the DappOperation
+    bytes data; // User operation calldata (used in `data` field of the user call)
+    bytes signature; // User operation signature signed by UserOperation.from
 }
