fe2d2ba49f89ca5f94b8f0e4b7ff9eaa3ff18518
Amitabh Anand <amitabh009@gmail.com>
2024-06-19 23:06:22 +0530

    remove sessionKey label in dappOp signer

--- src/contracts/types/DAppApprovalTypes.sol
+++ src/contracts/types/DAppApprovalTypes.sol
@@ -6,7 +6,7 @@ bytes32 constant DAPP_TYPEHASH = keccak256(
 );
 
 struct DAppOperation {
-    address from; // EOA of signer of the DAppOperation (SessionKey)
+    address from; // EOA of signer of the DAppOperation
     address to; // Atlas address
     uint256 nonce; // Atlas nonce of the DAppOperation available in the AtlasVerification contract
     uint256 deadline; // block.number deadline for the DAppOperation
@@ -14,7 +14,7 @@ struct DAppOperation {
     address bundler; // Signer of the atlas tx (msg.sender)
     bytes32 userOpHash; // keccak256 of userOp.to, userOp.data
     bytes32 callChainHash; // keccak256 of the solvers' txs
-    bytes signature; // DAppOperation signed by DAppOperation.from(SessionKey)
+    bytes signature; // DAppOperation signed by DAppOperation.from
 }
 
 struct DAppConfig {
