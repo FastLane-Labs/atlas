30fd3d37a421869d46fcc2d3c8dd0b52c3ff19db
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-06-21 11:51:58 +0200

    feat: add phase restriction to `borrow()` function

--- src/contracts/atlas/GasAccounting.sol
+++ src/contracts/atlas/GasAccounting.sol
@@ -30,9 +30,15 @@ abstract contract GasAccounting is SafetyLocks {
     }
 
     /// @notice Borrows ETH from the contract, transferring the specified amount to the caller if available.
+    /// @dev Borrowing is only available until the end of the SolverOperations phase, for solver protection.
     /// @param amount The amount of ETH to borrow.
     function borrow(uint256 amount) external payable {
-        if (lock.activeEnvironment != msg.sender) revert InvalidExecutionEnvironment(lock.activeEnvironment);
+        Lock memory currentLock = lock;
+        if (currentLock.activeEnvironment != msg.sender) {
+            revert InvalidExecutionEnvironment(currentLock.activeEnvironment);
+        }
+        if (currentLock.phase > ExecutionPhase.SolverOperations) revert WrongPhase();
+
         if (_borrow(amount)) {
             SafeTransferLib.safeTransferETH(msg.sender, amount);
         } else {
@@ -116,6 +122,9 @@ abstract contract GasAccounting is SafetyLocks {
     }
 
     /// @notice Borrows ETH from the contract, transferring the specified amount to the caller if available.
+    /// @dev Borrowing should never be allowed after the SolverOperations phase, for solver safety. This is enforced in
+    /// the external `borrow` function, and the only other time this internal `_borrow` function is called is in
+    /// `_trySolverLock` which happens at the beginning of the SolverOperations phase.
     /// @param amount The amount of ETH to borrow.
     /// @return valid A boolean indicating whether the borrowing operation was successful.
     function _borrow(uint256 amount) internal returns (bool valid) {
