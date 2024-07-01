2566f56be5bef9306b84c212f4584fe371d03c14
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-07-01 12:37:47 +0200

    fix: merge conflicts

--- src/contracts/examples/v4-example/UniV4Hook.sol
+++ src/contracts/examples/v4-example/UniV4Hook.sol
@@ -81,7 +81,7 @@ contract UniV4Hook is V4DAppControl {
 
             // Verify that the pool is valid for the user to trade in.
             require(keccak256(abi.encode(key, sender)) == hashLock, "ERR-H02 InvalidSwapper");
-        } else if (currentPhase == ExecutionPhase.SolverOperations) {
+        } else if (currentPhase == ExecutionPhase.SolverOperation) {
             // Case: Solver call
             // Sender = Solver contract
             // NOTE: This phase verifies that the user's transaction has already
