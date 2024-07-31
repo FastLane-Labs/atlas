b5757748eaaf2b3c78cb4848bb58d21e399d06b4
Alex <dalexwatts@gmail.com>
2024-07-31 16:12:46 -0700

    moved rake withdrawal to after setting its value to zero

--- src/contracts/examples/fastlane-online/OuterHelpers.sol
+++ src/contracts/examples/fastlane-online/OuterHelpers.sol
@@ -58,8 +58,9 @@ contract OuterHelpers is FastLaneOnlineInner {
 
     function makeThogardsWifeHappy() external onlyAsControl withUserLock {
         require(msg.sender == CARDANO_ENGINEER_THERAPY_FUND, "ERR - NOT MAD JUST DISAPPOINTED");
-        SafeTransferLib.safeTransferETH(CARDANO_ENGINEER_THERAPY_FUND, rake);
+        uint256 _rake = rake;
         rake = 0;
+        SafeTransferLib.safeTransferETH(CARDANO_ENGINEER_THERAPY_FUND, _rake);
     }
 
     function _getUserOperation(
