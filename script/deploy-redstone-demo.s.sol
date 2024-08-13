c875eebd54c03a35b09b5b08f1abb6cb805e58f5
Amitabh Anand <amitabh009@gmail.com>
2024-08-13 21:03:09 +0530

    honey pot changes and deployment

--- script/deploy-redstone-demo.s.sol
+++ script/deploy-redstone-demo.s.sol
@@ -8,6 +8,7 @@ import { RedstoneDAppControl } from "src/contracts/examples/redstone-oev/Redston
 import { RedstoneAdapterAtlasWrapper } from "src/contracts/examples/redstone-oev/RedstoneAdapterAtlasWrapper.sol";
 import { Atlas } from "src/contracts/atlas/Atlas.sol";
 import { AtlasVerification } from "src/contracts/atlas/AtlasVerification.sol";
+import { HoneyPot } from "src/contracts/examples/redstone-oev/HoneyPot.sol";
 
 contract DeployRedstoneDemoScript is DeployBaseScript {
     RedstoneDAppControl redstoneDAppControl;
@@ -55,5 +56,29 @@ contract DeployRedstoneDemoScript is DeployBaseScript {
         console.log("Contracts deployed by Oracle Owner:");
         console.log("Redstone Adapter Atlas Wrapper: \t\t\t", redstoneAdapterAtlasWrapper);
         console.log("\n");
+    }
+}
+
+contract DeployRedstoneHoneyPot is DeployBaseScript {
+    function run() external {
+        console.log("\n=== DEPLOYING REDSTONE HONEY POT ===\n");
+
+        uint256 honeyPotPrivateKey = vm.envUint("HONEYPOT_OWNER_PRIVATE_KEY");
+        address honeyPotOwner = vm.addr(honeyPotPrivateKey);
+
+        address oracle = vm.envAddress("ORACLE_ADDRESS");
+
+        console.log("Honey Pot Owner address: \t\t", honeyPotOwner);
+        console.log("Oracle address: \t\t\t", oracle);
+
+        vm.startBroadcast(honeyPotPrivateKey);
+        console.log("Deploying from Honey Pot Owner Account...");
+
+        HoneyPot honeyPot = new HoneyPot(honeyPotOwner, oracle);
+
+        vm.stopBroadcast();
+        console.log("Contracts deployed by Honey Pot Owner:");
+        console.log("Honey Pot: \t\t\t\t", address(honeyPot));
+        console.log("\n");        
     }
 }
--- src/contracts/examples/redstone-oev/HoneyPot.sol
+++ src/contracts/examples/redstone-oev/HoneyPot.sol
@@ -5,69 +5,48 @@ import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import { SafeTransferLib } from "solady/utils/SafeTransferLib.sol";
 
 interface IFeed {
-    function latestAnswer() external view returns (int256);
+    function latestRound() external view returns (uint80);
+    function getRoundData(uint80) external view returns (uint80, int256, uint256, uint256, uint80);
 }
 
 contract HoneyPot is Ownable {
-    struct HoneyPotDetails {
-        int256 liquidationPrice;
-        uint256 balance;
-    }
-
-    mapping(address => HoneyPotDetails) public honeyPots;
     address public oracle;
+    uint256 public MAX_LIQUIDATION_DELAY = 4;
+    uint8 public LIQUIDATION_HANDOUT_PERCENT = 10;
 
-    event OracleUpdated(address indexed newOracle);
-    event HoneyPotCreated(address indexed owner, int256 initialPrice, uint256 amount);
-    event HoneyPotEmptied(address indexed owner, address indexed liquidator, uint256 amount);
-    event HoneyPotReset(address indexed owner, uint256 amount);
+    event HoneyPotLiquidation(address indexed liquidator, uint256 amountPaid);
 
-    constructor(address _oracle) {
+    constructor(address _owner, address _oracle) {
         oracle = _oracle;
+        _transferOwnership(_owner);
+    }
+    receive() external payable {}
+
+    function pay(address recipient, uint256 amount) external onlyOwner {
+        SafeTransferLib.safeTransferETH(recipient, amount);
     }
 
     function setOracle(address _oracle) external onlyOwner {
         oracle = _oracle;
-        emit OracleUpdated(address(_oracle));
     }
 
-    function createHoneyPot() external payable {
-        require(honeyPots[msg.sender].liquidationPrice == 0, "Liquidation price already set for this user");
-        require(msg.value > 0, "No value sent");
-
-        int256 currentPrice = IFeed(oracle).latestAnswer();
-
-        honeyPots[msg.sender].liquidationPrice = currentPrice;
-        honeyPots[msg.sender].balance = msg.value;
-
-        emit HoneyPotCreated(msg.sender, currentPrice, msg.value);
+    function setLiquidationDelay(uint256 _delay) external onlyOwner {
+        MAX_LIQUIDATION_DELAY = _delay;
     }
 
-    function _emptyPotForUser(address owner, address recipient) internal returns (uint256 amount) {
-        HoneyPotDetails storage userPot = honeyPots[owner];
-
-        amount = userPot.balance;
-        userPot.balance = 0; // reset the balance
-        userPot.liquidationPrice = 0; // reset the liquidation price
-
-        SafeTransferLib.safeTransferETH(payable(recipient), amount);
+    function setLiquidationHandoutPercent(uint8 _percent) external onlyOwner {
+        LIQUIDATION_HANDOUT_PERCENT = _percent;
     }
 
-    function emptyHoneyPot(address owner) external {
-        int256 currentPrice = IFeed(oracle).latestAnswer();
-        require(currentPrice >= 0, "Invalid price from oracle");
+    function liquidate() external {
+        (,int256 ans,uint256 oracleTimestamp,,) = IFeed(oracle).getRoundData(IFeed(oracle).latestRound());
+        require(block.timestamp - oracleTimestamp <= MAX_LIQUIDATION_DELAY, "HoneyPot: Liquidation delay exceeded");
+        require(ans > 0, "HoneyPot: Oracle answer is not positive");
 
-        HoneyPotDetails storage userPot = honeyPots[owner];
+        uint256 toPay = address(this).balance * LIQUIDATION_HANDOUT_PERCENT / 100;
+        require(toPay > 0, "HoneyPot: Nothing to liquidate");
 
-        require(currentPrice != userPot.liquidationPrice, "Liquidation price reached for this user");
-        require(userPot.balance > 0, "No balance to withdraw");
-
-        uint256 withdrawnAmount = _emptyPotForUser(owner, msg.sender);
-        emit HoneyPotEmptied(owner, msg.sender, withdrawnAmount);
-    }
-
-    function resetPot() external {
-        uint256 withdrawnAmount = _emptyPotForUser(msg.sender, msg.sender);
-        emit HoneyPotReset(msg.sender, withdrawnAmount);
+        SafeTransferLib.safeTransferETH(msg.sender, toPay);
+        emit HoneyPotLiquidation(msg.sender, toPay);
     }
 }
