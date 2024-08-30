7c355ef053e9661cf752790933f698c5f770642a
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-08-30 15:31:55 +0200

    test: refactor swapCompact calldata builder

--- test/TrebleSwap.t.sol
+++ test/TrebleSwap.t.sol
@@ -104,18 +104,12 @@ contract TrebleSwapTest is BaseTest {
         });
         vm.roll(args.blockBefore);
 
-        // Modify swapCompact() calldata to replace original caller (0xa4a9220dE44D699f453DdF7F7630A96cDEdf6463) with
-        // user's Execution Environment address:
-        bytes memory calldataPart1 =
-            hex"83bd37f9000400014da78059d97f155e18b37765e2e042270f4e0fc4040bc108800601d1d9f50a5a028f5c0001f73f77f9466da712590ae432a80f07fd50a7de600001616535324976f8dbcef19df0705b95ace86ebb480001";
-        bytes memory calldataPart2 =
-            hex"0000000006020207003401000001020180000005020a0004040500000301010003060119ff0000000000000000000000000000000000000000000000000000000000000000616535324976f8dbcef19df0705b95ace86ebb48833589fcd6edb6e08f4c7c32d4f71b54bda02913569d81c17b5b4ac08929dc1769b8e39668d3ae29f6c0a374a483101e04ef5f7ac9bd15d9142bac95d9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca42000000000000000000000000000000000000060000000000000000";
-        bytes memory swapCompactCalldata = abi.encodePacked(calldataPart1, executionEnvironment, calldataPart2);
-
-        _checkActualCalldataMatchesExpected(swapCompactCalldata);
-        _buildUserOp(swapCompactCalldata);
+        bytes memory userOpData = _buildUserOpData("Erc20ToErc20");
+        _checkActualCalldataMatchesExpected(userOpData);
+        _buildUserOp(userOpData);
         // no solverOps
         _buildDAppOp();
+
         _setBalancesAndApprovals();
         _checkSimulationsPass();
         _doMetacallAndChecks({ winningSolver: address(0) });
@@ -136,22 +130,13 @@ contract TrebleSwapTest is BaseTest {
         });
         vm.roll(args.blockBefore);
 
-        // Modify swapCompact() calldata to replace original caller (0xa4a9220dE44D699f453DdF7F7630A96cDEdf6463) with
-        // user's Execution Environment address:
-        bytes memory calldataPart1 =
-            hex"83bd37f9000400014da78059d97f155e18b37765e2e042270f4e0fc4040bc108800601d1d9f50a5a028f5c0001f73f77f9466da712590ae432a80f07fd50a7de600001616535324976f8dbcef19df0705b95ace86ebb480001";
-        bytes memory calldataPart2 =
-            hex"0000000006020207003401000001020180000005020a0004040500000301010003060119ff0000000000000000000000000000000000000000000000000000000000000000616535324976f8dbcef19df0705b95ace86ebb48833589fcd6edb6e08f4c7c32d4f71b54bda02913569d81c17b5b4ac08929dc1769b8e39668d3ae29f6c0a374a483101e04ef5f7ac9bd15d9142bac95d9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca42000000000000000000000000000000000000060000000000000000";
-        bytes memory swapCompactCalldata = abi.encodePacked(calldataPart1, executionEnvironment, calldataPart2);
-
-        _checkActualCalldataMatchesExpected(swapCompactCalldata);
-
-        _buildUserOp(swapCompactCalldata);
+        bytes memory userOpData = _buildUserOpData("Erc20ToErc20");
+        _checkActualCalldataMatchesExpected(userOpData);
+        _buildUserOp(userOpData);
         address solverContract = _setUpSolver(solverOneEOA, solverOnePK, 1e18);
         _buildDAppOp();
 
         _setBalancesAndApprovals();
-
         _checkSimulationsPass();
         _doMetacallAndChecks({ winningSolver: solverContract });
     }
@@ -171,18 +156,12 @@ contract TrebleSwapTest is BaseTest {
         });
         vm.roll(args.blockBefore);
 
-        // Modify swapCompact() calldata to replace original caller (0xadF6918eD87a5D7aE334bB42Ca2d98971B527306) with
-        // user's Execution Environment address:
-        bytes memory calldataPart1 =
-            hex"83bd37f9000000050801b505fc9226ffb80910d5345f06a9650000028f5c0001f73f77f9466da712590ae432a80f07fd50a7de6000000001";
-        bytes memory calldataPart2 =
-            hex"000000000401020500040101020a000202030100340101000104007ffffffaff00000000005fb33b095c6e739be19364ab408cd8f102262bb672ab388e2e2f6facef59e3c3fa2c4e29011c2d384200000000000000000000000000000000000006833589fcd6edb6e08f4c7c32d4f71b54bda0291300000000000000000000000000000000";
-        bytes memory swapCompactCalldata = abi.encodePacked(calldataPart1, executionEnvironment, calldataPart2);
-
-        _checkActualCalldataMatchesExpected(swapCompactCalldata);
-        _buildUserOp(swapCompactCalldata);
+        bytes memory userOpData = _buildUserOpData("EthToErc20");
+        _checkActualCalldataMatchesExpected(userOpData);
+        _buildUserOp(userOpData);
         // no solverOps
         _buildDAppOp();
+
         _setBalancesAndApprovals();
         _checkSimulationsPass();
         _doMetacallAndChecks({ winningSolver: address(0) });
@@ -203,18 +182,12 @@ contract TrebleSwapTest is BaseTest {
         });
         vm.roll(args.blockBefore);
 
-        // Modify swapCompact() calldata to replace original caller (0x24971B34b7d4e4c880983e4ebc015558af5FF7dB) with
-        // user's Execution Environment address:
-        bytes memory calldataPart1 =
-            hex"83bd37f90001532f27101965dd16442e59d40670faf5ebb142e400000a05421c0933c565400000080aed2177322a39000041890001f73f77f9466da712590ae432a80f07fd50a7de6000000001";
-        bytes memory calldataPart2 =
-            hex"0000000003010204010e6604680a0100010200000a0100030200020400000001ff0000000036a46dff597c5a444bbc521d26787f57867d2214532f27101965dd16442e59d40670faf5ebb142e44e829f8a5213c42535ab84aa40bd4adcce9cba0200000000";
-        bytes memory swapCompactCalldata = abi.encodePacked(calldataPart1, executionEnvironment, calldataPart2);
-
-        _checkActualCalldataMatchesExpected(swapCompactCalldata);
-        _buildUserOp(swapCompactCalldata);
+        bytes memory userOpData = _buildUserOpData("Erc20ToEth");
+        _checkActualCalldataMatchesExpected(userOpData);
+        _buildUserOp(userOpData);
         // no solverOps
         _buildDAppOp();
+
         _setBalancesAndApprovals();
         _checkSimulationsPass();
         _doMetacallAndChecks({ winningSolver: address(0) });
@@ -291,7 +264,7 @@ contract TrebleSwapTest is BaseTest {
         }
     }
 
-    function _checkActualCalldataMatchesExpected(bytes memory userOpData) internal {
+    function _checkActualCalldataMatchesExpected(bytes memory userOpData) internal view {
         bytes memory encodedCall = abi.encodePacked(TrebleSwapDAppControl.decodeUserOpData.selector, userOpData);
         (bool res, bytes memory returnData) = address(trebleSwapControl).staticcall(encodedCall);
         assertEq(res, true, "calldata check failed in decode call");
@@ -414,6 +387,34 @@ contract TrebleSwapTest is BaseTest {
 
         (sig.v, sig.r, sig.s) = vm.sign(governancePK, atlasVerification.getDAppOperationPayload(args.dAppOp));
         args.dAppOp.signature = abi.encodePacked(sig.r, sig.s, sig.v);
+    }
+
+    function _buildUserOpData(string memory swapType) internal view returns (bytes memory) {
+        bytes32 typeHash = keccak256(abi.encodePacked(swapType));
+        bytes memory calldataPart1;
+        bytes memory calldataPart2;
+        if (typeHash == keccak256("Erc20ToErc20")) {
+            // Tx: https://basescan.org/tx/0x0ef4a9c24bbede2b39e12f5e5417733fa8183f372e41ee099c2c7523064c1b55
+            calldataPart1 =
+                hex"83bd37f9000400014da78059d97f155e18b37765e2e042270f4e0fc4040bc108800601d1d9f50a5a028f5c0001f73f77f9466da712590ae432a80f07fd50a7de600001616535324976f8dbcef19df0705b95ace86ebb480001";
+            calldataPart2 =
+                hex"0000000006020207003401000001020180000005020a0004040500000301010003060119ff0000000000000000000000000000000000000000000000000000000000000000616535324976f8dbcef19df0705b95ace86ebb48833589fcd6edb6e08f4c7c32d4f71b54bda02913569d81c17b5b4ac08929dc1769b8e39668d3ae29f6c0a374a483101e04ef5f7ac9bd15d9142bac95d9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca42000000000000000000000000000000000000060000000000000000";
+        } else if (typeHash == keccak256("EthToErc20")) {
+            // Tx: https://basescan.org/tx/0xe138def4155bea056936038b9374546a366828ab8bf1233056f9e2fe4c6af999
+            calldataPart1 =
+                hex"83bd37f9000000050801b505fc9226ffb80910d5345f06a9650000028f5c0001f73f77f9466da712590ae432a80f07fd50a7de6000000001";
+            calldataPart2 =
+                hex"000000000401020500040101020a000202030100340101000104007ffffffaff00000000005fb33b095c6e739be19364ab408cd8f102262bb672ab388e2e2f6facef59e3c3fa2c4e29011c2d384200000000000000000000000000000000000006833589fcd6edb6e08f4c7c32d4f71b54bda0291300000000000000000000000000000000";
+        } else if (typeHash == keccak256("Erc20ToEth")) {
+            // https://basescan.org/tx/0xaf26570fceddf2d21219a9e03f2cfee52c600a40ddfdfc5d82eff14f3d322f8f
+            calldataPart1 =
+                hex"83bd37f90001532f27101965dd16442e59d40670faf5ebb142e400000a05421c0933c565400000080aed2177322a39000041890001f73f77f9466da712590ae432a80f07fd50a7de6000000001";
+            calldataPart2 =
+                hex"0000000003010204010e6604680a0100010200000a0100030200020400000001ff0000000036a46dff597c5a444bbc521d26787f57867d2214532f27101965dd16442e59d40670faf5ebb142e44e829f8a5213c42535ab84aa40bd4adcce9cba0200000000";
+        } else {
+            revert("Invalid swap type");
+        }
+        return abi.encodePacked(calldataPart1, executionEnvironment, calldataPart2);
     }
 
     function _balanceOf(address token, address account) internal view returns (uint256) {
