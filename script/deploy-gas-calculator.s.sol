c4de266110dd7c3de7db2f3aa6ab831e42310564
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-09-13 10:05:52 +0200

    chore: Base L2 Gas Calculator deploy script tweaks

--- script/deploy-gas-calculator.s.sol
+++ script/deploy-gas-calculator.s.sol
@@ -7,16 +7,25 @@ import { DeployBaseScript } from "script/base/deploy-base.s.sol";
 import { BaseGasCalculator } from "src/contracts/gasCalculator/BaseGasCalculator.sol";
 
 contract DeployGasCalculatorScript is DeployBaseScript {
+    // NOTE: Adjust the constructor parameters as needed here:
+    // - BASE_GAS_PRICE_ORACLE: The address of the gas price oracle contract
+    // - BASE_CALLDATA_LENGTH_OFFSET: The offset to be applied to the calldata length (can be negative or positive)
+    // -----------------------------------------------------------------------------------------------
+    address constant BASE_GAS_PRICE_ORACLE = address(0x420000000000000000000000000000000000000F);
+    int256 constant BASE_CALLDATA_LENGTH_OFFSET = 0; // can be negative or positive
+    // -----------------------------------------------------------------------------------------------
+
     function run() external {
         console.log("\n=== DEPLOYING GasCalculator ===\n");
 
         console.log("Deploying to chain: \t\t", _getDeployChain());
 
-        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("GOV_PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
 
+        console.log("Deployer address: \t\t", deployer);
+
         uint256 chainId = block.chainid;
-        string memory deploymentName;
         address deploymentAddr;
 
         vm.startBroadcast(deployerPrivateKey);
@@ -24,10 +33,9 @@ contract DeployGasCalculatorScript is DeployBaseScript {
         if (chainId == 8453 || chainId == 84_532) {
             // Base or Base Sepolia
             BaseGasCalculator gasCalculator = new BaseGasCalculator({
-                _gasPriceOracle: address(0), // Insert gas price oracle address here
-                _calldataLengthOffset: 0 // Insert calldata length offset here (can be negative)
-             });
-            deploymentName = "BASE_GAS_CALCULATOR";
+                _gasPriceOracle: BASE_GAS_PRICE_ORACLE,
+                _calldataLengthOffset: BASE_CALLDATA_LENGTH_OFFSET
+            });
             deploymentAddr = address(gasCalculator);
         } else {
             revert("Error: Chain ID not supported");
@@ -35,11 +43,14 @@ contract DeployGasCalculatorScript is DeployBaseScript {
 
         vm.stopBroadcast();
 
-        _writeAddressToDeploymentsJson(deploymentName, deploymentAddr);
+        _writeAddressToDeploymentsJson("L2_GAS_CALCULATOR", deploymentAddr);
 
         console.log("\n");
-        console.log("Deployed contract: ", deploymentName);
-        console.log("Deployed at address: ", deploymentAddr);
+        console.log("-------------------------------------------------------------------------------");
+        console.log("| Contract                    | Address                                       |");
+        console.log("-------------------------------------------------------------------------------");
+        console.log("| L2_GAS_CALCULATOR (Base)    | ", address(deploymentAddr), "  |");
+        console.log("-------------------------------------------------------------------------------");
         console.log("\n");
         console.log("You can find a list of contract addresses from the latest deployment in deployments.json");
     }
