b9e6bc9a4424de337d7647767b0704c57690e2c7
0xNotMe <nme15500@gmail.com>
2024-10-04 17:46:10 +1000

    fix: Add Arbitrum SEPOLIA support

--- src/contracts/libraries/SafeBlockNumber.sol
+++ src/contracts/libraries/SafeBlockNumber.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.13;
 
 import { ArbSys } from "nitro-contracts/src/precompiles/ArbSys.sol";
-import { console } from "forge-std/console.sol";
 
 library SafeBlockNumber {
-    ArbSys internal constant arbsys = ArbSys(address(0x0000000000000000000000000000000000000064));
+    ArbSys internal constant ARB_SYS = ArbSys(address(0x0000000000000000000000000000000000000064));
 
     uint256 internal constant ARBITRUM_ONE_CHAIN_ID = 42_161;
     uint256 internal constant ARBITRUM_NOVA_CHAIN_ID = 42_170;
+    uint256 internal constant ARBITRUM_SEPOLIA_CHAIN_ID = 421_614;
 
     function get() internal view returns (uint256) {
-        if (block.chainid == ARBITRUM_ONE_CHAIN_ID || block.chainid == ARBITRUM_NOVA_CHAIN_ID) {
+        uint256 chainId = block.chainid;
+        if (chainId == ARBITRUM_ONE_CHAIN_ID || chainId == ARBITRUM_NOVA_CHAIN_ID || chainId == ARBITRUM_SEPOLIA_CHAIN_ID) {
             // Arbitrum One or Nova chain
-            return arbsys.arbBlockNumber();
+            return ARB_SYS.arbBlockNumber();
         } else {
             return block.number;
         }
