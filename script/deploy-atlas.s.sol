afd923076c6a39d7b30c1d16b70c193a832c4ce2
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-10-24 15:00:49 +0200

    refactor: L2 Gas Calc deploy logic and helpers

--- script/deploy-atlas.s.sol
+++ script/deploy-atlas.s.sol
@@ -33,9 +33,9 @@ contract DeployAtlasScript is DeployBaseScript, GasCalculatorDeployHelper {
         address expectedAtlasVerificationAddr = vm.computeCreateAddress(deployer, vm.getNonce(deployer) + 3);
         address expectedSimulatorAddr = vm.computeCreateAddress(deployer, vm.getNonce(deployer) + 4);
 
-        // Deploy gas calculator for Arbitrum chains after other deployments
-        address l2GasCalculatorAddr = _predictGasCalculatorAddress(deployer, vm.getNonce(deployer) + 5);
-        // TODO return either addr(0) or predicted addr, depending on chainid. From GasCalculatorDeployer
+        // Deploy L2 Gas Calculator after other deployments
+        address expectedL2GasCalculatorAddr = _predictGasCalculatorAddress(deployer, vm.getNonce(deployer) + 5);
+        address actualL2GasCalculatorAddr; // Set below during deployment if chain is an L2
 
         address prevSimAddr = _getAddressFromDeploymentsJson("SIMULATOR");
         uint256 prevSimBalance = (prevSimAddr == address(0)) ? 0 : prevSimAddr.balance;
@@ -56,7 +56,7 @@ contract DeployAtlasScript is DeployBaseScript, GasCalculatorDeployHelper {
             simulator: expectedSimulatorAddr,
             factoryLib: address(factoryLib),
             initialSurchargeRecipient: deployer,
-            l2GasCalculator: l2GasCalculatorAddr // is address(0) if not on Arbitrum
+            l2GasCalculator: expectedL2GasCalculatorAddr // address(0) if chain not an L2
          });
         atlasVerification = new AtlasVerification(address(atlas));
 
@@ -70,13 +70,10 @@ contract DeployAtlasScript is DeployBaseScript, GasCalculatorDeployHelper {
 
         sorter = new Sorter(address(atlas));
 
-        // TODO refactor to a fn in GasCalculatorDeployer
-        // if (chainId == 42_161 || chainId == 42_170 || chainId == 421_614) {
-        //     // Deploy gas calculator for Arbitrum chains
-        //     console.log("Deploying L2 Gas Calculator at: ", l2GasCalculatorAddr);
-        //     DeployGasCalculatorScript gasCalculatorDeployer = new DeployGasCalculatorScript();
-        //     l2GasCalculatorAddr = gasCalculatorDeployer.deployL2GasCalculator();
-        // }
+        // If chain is an L2, expectedL2GasCalculatorAddr will be non-zero
+        if (expectedL2GasCalculatorAddr != address(0)) {
+            actualL2GasCalculatorAddr = _newGasCalculator();
+        }
 
         vm.stopBroadcast();
 
@@ -136,9 +133,13 @@ contract DeployAtlasScript is DeployBaseScript, GasCalculatorDeployHelper {
             console.log("ERROR: ExecutionEnvironment address not set correctly in FactoryLib");
             error = true;
         }
-        // Check GasCalculator address set correctly in Atlas
-        if (l2GasCalculatorAddr != atlas.L2_GAS_CALCULATOR()) {
+        // Check L2 Gas Calculator address set correctly
+        if (actualL2GasCalculatorAddr != atlas.L2_GAS_CALCULATOR()) {
             console.log("ERROR: L2 Gas Calculator address not set correctly in Atlas");
+            error = true;
+        }
+        if (actualL2GasCalculatorAddr != expectedL2GasCalculatorAddr) {
+            console.log("ERROR: L2 Gas Calculator deployment address not as expected");
             error = true;
         }
         // Check ESCROW_DURATION was not set to 0
@@ -156,8 +157,8 @@ contract DeployAtlasScript is DeployBaseScript, GasCalculatorDeployHelper {
         _writeAddressToDeploymentsJson("ATLAS_VERIFICATION", address(atlasVerification));
         _writeAddressToDeploymentsJson("SIMULATOR", address(simulator));
         _writeAddressToDeploymentsJson("SORTER", address(sorter));
-        if (l2GasCalculatorAddr != address(0)) {
-            _writeAddressToDeploymentsJson("L2_GAS_CALCULATOR", l2GasCalculatorAddr);
+        if (actualL2GasCalculatorAddr != address(0)) {
+            _writeAddressToDeploymentsJson("L2_GAS_CALCULATOR", actualL2GasCalculatorAddr);
         }
 
         // Print the table header
@@ -169,8 +170,8 @@ contract DeployAtlasScript is DeployBaseScript, GasCalculatorDeployHelper {
         console.log("| AtlasVerification     | ", address(atlasVerification), " |");
         console.log("| Simulator             | ", address(simulator), " |");
         console.log("| Sorter                | ", address(sorter), " |");
-        if (l2GasCalculatorAddr != address(0)) {
-            console.log("| L2 Gas Calculator     | ", l2GasCalculatorAddr, " |");
+        if (actualL2GasCalculatorAddr != address(0)) {
+            console.log("| L2 Gas Calculator     | ", actualL2GasCalculatorAddr, " |");
         }
         console.log("------------------------------------------------------------------------");
         console.log("\n");
--- script/deploy-gas-calculator.s.sol
+++ script/deploy-gas-calculator.s.sol
@@ -7,32 +7,71 @@ import { DeployBaseScript } from "script/base/deploy-base.s.sol";
 import { ArbitrumGasCalculator } from "src/contracts/gasCalculator/ArbitrumGasCalculator.sol";
 import { BaseGasCalculator } from "src/contracts/gasCalculator/BaseGasCalculator.sol";
 
-contract GasCalculatorDeployHelper {
-    function _newGasCalculator() internal returns (address) {
-        // TODO
-        return address(0);
-    }
-
-    function _predictGasCalculatorAddress(address deployer, uint256 deployNonce) internal returns (address) {
-        // TODO
-        return address(0);
-    }
-}
-
-contract DeployGasCalculatorScript is GasCalculatorDeployHelper, DeployBaseScript {
-    // TODO this should also move to helper contract that creates new contract
+contract GasCalculatorDeployHelper is Test {
     // NOTE: Adjust the constructor parameters as needed here:
     // -----------------------------------------------------------------------------------------------
     // Base:
     // -----------------------------------------------------------------------------------------------
+    uint256 constant BASE_SEPOLIA = 8453;
+    uint256 constant BASE_MAINNET = 84_532;
     address constant BASE_GAS_PRICE_ORACLE = address(0x420000000000000000000000000000000000000F);
     int256 constant BASE_CALLDATA_LENGTH_OFFSET = 0; // can be negative or positive
     // -----------------------------------------------------------------------------------------------
     // Arbitrum:
     // -----------------------------------------------------------------------------------------------
+    uint256 constant ARBITRUM_SEPOLIA = 421_614;
+    uint256 constant ARBITRUM_ONE = 42_161;
+    uint256 constant ARBITRUM_NOVA = 42_170;
     int256 constant ARBITRUM_CALLDATA_LENGTH_OFFSET = 0;
     // -----------------------------------------------------------------------------------------------
 
+    function _newGasCalculator() internal returns (address l2GasCalculatorAddr) {
+        uint256 chainId = block.chainid;
+
+        if (chainId == BASE_MAINNET || chainId == BASE_SEPOLIA) {
+            // Base
+            BaseGasCalculator gasCalculator = new BaseGasCalculator({
+                gasPriceOracle: BASE_GAS_PRICE_ORACLE,
+                calldataLenOffset: BASE_CALLDATA_LENGTH_OFFSET
+            });
+            l2GasCalculatorAddr = address(gasCalculator);
+        } else if (chainId == ARBITRUM_ONE || chainId == ARBITRUM_SEPOLIA) {
+            // Arbitrum One or Arbitrum Sepolia
+            ArbitrumGasCalculator gasCalculator = new ArbitrumGasCalculator({
+                calldataLenOffset: ARBITRUM_CALLDATA_LENGTH_OFFSET,
+                _isArbitrumNova: false
+            });
+            l2GasCalculatorAddr = address(gasCalculator);
+        } else if (chainId == ARBITRUM_NOVA) {
+            // Arbitrum Nova
+            ArbitrumGasCalculator gasCalculator =
+                new ArbitrumGasCalculator({ calldataLenOffset: ARBITRUM_CALLDATA_LENGTH_OFFSET, _isArbitrumNova: true });
+            l2GasCalculatorAddr = address(gasCalculator);
+        } else {
+            revert("Error: Chain ID not supported for L2 Gas Calculator deployment");
+        }
+    }
+
+    /// @notice Returns the predicted address of the gas calculator contract, given the nonce of the deployment tx. Or
+    /// returns address(0) if the current chain does not require an L2 Gas Calculator.
+    function _predictGasCalculatorAddress(address deployer, uint256 deployNonce) internal returns (address) {
+        if (_chainNeedsL2GasCalculator()) {
+            return vm.computeCreateAddress(deployer, deployNonce);
+        }
+
+        // If not one of the supported chains above, return address(0)
+        return address(0);
+    }
+
+    function _chainNeedsL2GasCalculator() internal returns (bool) {
+        uint256 chainId = block.chainid;
+
+        return (chainId == BASE_MAINNET || chainId == BASE_SEPOLIA)
+            || (chainId == ARBITRUM_ONE || chainId == ARBITRUM_SEPOLIA || chainId == ARBITRUM_NOVA);
+    }
+}
+
+contract DeployGasCalculatorScript is DeployBaseScript, GasCalculatorDeployHelper {
     function run() external {
         console.log("\n=== DEPLOYING Gas Calculator ===\n");
         console.log("Deploying to chain: \t\t", _getDeployChain());
@@ -58,38 +97,3 @@ contract DeployGasCalculatorScript is GasCalculatorDeployHelper, DeployBaseScrip
         console.log("You can find the contract address in deployments.json");
     }
 }
-
-// For deploy helper
-// if (chainId == 8453 || chainId == 84_532) {
-//             // Base or Base Sepolia
-//             BaseGasCalculator gasCalculator = new BaseGasCalculator({
-//                 gasPriceOracle: BASE_GAS_PRICE_ORACLE,
-//                 calldataLenOffset: BASE_CALLDATA_LENGTH_OFFSET
-//             });
-//             deploymentAddr = address(gasCalculator);
-//         } else {
-//             revert("Error: Chain ID not supported");
-//         }
-
-// function deployL2GasCalculator() public returns (address) {
-//         uint256 chainId = block.chainid;
-//         address deploymentAddr;
-
-//         if (chainId == 42_161 || chainId == 421_614) {
-//             // Arbitrum One or Arbitrum Sepolia
-//             ArbitrumGasCalculator gasCalculator = new ArbitrumGasCalculator({
-//                 calldataLenOffset: ARBITRUM_CALLDATA_LENGTH_OFFSET,
-//                 _isArbitrumNova: false
-//             });
-//             deploymentAddr = address(gasCalculator);
-//         } else if (chainId == 42_170) {
-//             // Arbitrum Nova
-//             ArbitrumGasCalculator gasCalculator =
-//                 new ArbitrumGasCalculator({ calldataLenOffset: ARBITRUM_CALLDATA_LENGTH_OFFSET, _isArbitrumNova: true
-// });
-//             deploymentAddr = address(gasCalculator);
-//         } else {
-//             revert("Error: Chain ID not supported for gas calculator deployment");
-//         }
-
-//         return deploymentAddr;
