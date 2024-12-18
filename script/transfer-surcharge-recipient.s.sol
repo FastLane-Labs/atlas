21ff283d8b8a063de47b9c41596dc6531f3357e9
Alexander Fadeev <fadeevab.com@gmail.com>
2024-12-18 04:21:48 +0200

    Fix linter + comments

--- script/transfer-surcharge-recipient.s.sol
+++ script/transfer-surcharge-recipient.s.sol
@@ -7,18 +7,19 @@ import { DeployBaseScript } from "script/base/deploy-base.s.sol";
 
 import { Atlas } from "src/contracts/atlas/Atlas.sol";
 
+/// @dev npm run transfer-surcharge-recipient:sepolia -- <address>
 contract TransferSurchargeRecipientScript is DeployBaseScript {
     function run(string calldata _newRecipient) external {
         console.log("\n=== Transferring Atlas Surcharge Recipient ===\n");
 
-        // string hex to address
+        // Hex string to address.
         address newRecipient = vm.parseAddress(_newRecipient);
 
-        // Get the deployer's private key from environment
+        // Get the deployer's private key from environment.
         uint256 deployerPrivateKey = vm.envUint("GOV_PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
 
-        // Get Atlas contract address from deployments
+        // Get Atlas contract address from deployments.
         address atlasAddress = _getAddressFromDeploymentsJson(".ATLAS");
         Atlas atlas = Atlas(payable(atlasAddress));
 
@@ -26,7 +27,7 @@ contract TransferSurchargeRecipientScript is DeployBaseScript {
         console.log("Atlas address: \t\t\t\t", atlasAddress);
         console.log("New surcharge recipient: \t\t\t", newRecipient);
 
-        // Transfer the surcharge recipient role to the new address
+        // Transfer the surcharge recipient role to the new address.
         vm.startBroadcast(deployerPrivateKey);
         atlas.transferSurchargeRecipient(newRecipient);
         vm.stopBroadcast();
