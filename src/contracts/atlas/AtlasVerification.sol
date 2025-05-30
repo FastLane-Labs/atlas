b113454d40875ab18358ba044eae182862304a7e
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2025-05-30 20:42:01 +0400

    chore: bump Arbitrum Atlas version to v1.6.1

--- src/contracts/atlas/AtlasVerification.sol
+++ src/contracts/atlas/AtlasVerification.sol
@@ -36,7 +36,7 @@ contract AtlasVerification is EIP712, NonceManager, DAppIntegration {
         address atlas,
         address l2GasCalculator
     )
-        EIP712("AtlasVerification", "1.6")
+        EIP712("AtlasVerification", "1.6.1")
         DAppIntegration(atlas, l2GasCalculator)
     { }
 
