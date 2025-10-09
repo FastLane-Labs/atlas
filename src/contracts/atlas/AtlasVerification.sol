c76196a8d573328e2925238e1d5fd60fc86a4070
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2025-10-09 10:30:48 +0400

    chore: bump Atlas to `v1.6.4`

--- src/contracts/atlas/AtlasVerification.sol
+++ src/contracts/atlas/AtlasVerification.sol
@@ -36,7 +36,7 @@ contract AtlasVerification is EIP712, NonceManager, DAppIntegration {
         address atlas,
         address l2GasCalculator
     )
-        EIP712("AtlasVerification", "1.6.3")
+        EIP712("AtlasVerification", "1.6.4")
         DAppIntegration(atlas, l2GasCalculator)
     { }
 
